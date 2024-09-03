6d6216d462098c7848ccf66a01b3e15712d3da90
한지웅 <wldnd2977@gmail.com>
2024-09-03 10:08:56 +0900

    [Design] NavBar

--- frontend/frontend_pjt/src/app/(afterlogin)/layout.tsx
+++ frontend/frontend_pjt/src/app/(afterlogin)/layout.tsx
@@ -1,2 +1,18 @@
 // 로그인 한 이후 화면 구성을 담습니다.
 // 하단 bar 구성해야함.
+import NavBar from "./_component/NavBar"
+
+type Props = {
+    children: React.ReactNode
+}
+
+export default function RootLayout({children}: Props) {
+    return (
+        <html lang="en">
+        <body>
+            {children}
+            <nav><NavBar/></nav>
+        </body>
+        </html>
+    );
+}
