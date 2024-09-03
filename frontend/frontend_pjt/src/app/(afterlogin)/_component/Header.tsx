009bb6ee8a280d678783022374053a63f90121bc
이주영 <git.leej.young@gmail.com>
2024-09-03 10:59:02 +0900

    [Design] Header 생성

--- frontend/frontend_pjt/src/app/(afterlogin)/_component/Header.tsx
+++ frontend/frontend_pjt/src/app/(afterlogin)/_component/Header.tsx
@@ -1,42 +1,33 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import GoBack from "./(SVG_component)/GoBack";
+import Search from "./(SVG_component)/Search";
+import Notification from "./(SVG_component)/Notification";
 
 type HeaderProps = {
   pageName: string;
 };
 
 export default function Header({ pageName }: HeaderProps) {
+  const handleGoBack = () => {
+    window.history.back();
+  };
+
   return (
     <header className="bg-white w-full h-[7vh] flex items-center p-4">
       <div className="flex flex-grow">
-        <Link href="/prev" className="mx-1 ml-0">
-          {/* Image 태그 전부 svg 컴포로 대체해야 함 */}
-          <Image
-            src="/assets/svg/go_back.svg"
-            alt="prev"
-            width={24}
-            height={24}
-          />
-        </Link>
+        <div onClick={handleGoBack}>
+          <GoBack />
+        </div>
         <span className="flex-grow text-left mx-5 ml-0">{pageName}</span>
       </div>
       <div className="flex flex-none items-center space-x-4">
         <Link href="/search">
-          <Image
-            src="/assets/svg/search.svg"
-            alt="Search"
-            width={24}
-            height={24}
-          />
+          <Search />
         </Link>
         <Link href="/alert">
-          <Image
-            src="/assets/svg/notification.svg"
-            alt="Alert"
-            width={24}
-            height={24}
-          />
+          <Notification />
         </Link>
       </div>
     </header>
