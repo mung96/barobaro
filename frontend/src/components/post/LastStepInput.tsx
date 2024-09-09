b0f4641903393d728a6448d558aaf5748729b142
mung96 <mung5079@gmail.com>
2024-09-09 16:33:31 +0900

    feat: 이전으로 가는 페이지 전환 애니메이션 구현

--- frontend/src/components/post/LastStepInput.tsx
+++ frontend/src/components/post/LastStepInput.tsx
@@ -1,7 +1,14 @@
-function LastStepInput() {
+type LastStepInputProps = {
+  onPrev: () => void;
+};
+
+function LastStepInput({ onPrev }: LastStepInputProps) {
   return (
-    <div>
+    <div className="flex flex-col">
       <h2>게시글 등록 마지막</h2>
+      <button type="button" onClick={onPrev}>
+        이전 스탭으로 돌아가기
+      </button>
       <button type="button">등록이 완료됐어요.</button>
     </div>
   );
