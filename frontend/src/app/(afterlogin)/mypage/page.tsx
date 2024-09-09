1b35c62198e4c239693a590802150e9d38a43750
한지웅 <wldnd2977@gmail.com>
2024-09-09 13:55:39 +0900

    refactor : search enter svg => SearchEnter.tsx

--- frontend/src/app/(afterlogin)/mypage/page.tsx
+++ frontend/src/app/(afterlogin)/mypage/page.tsx
@@ -2,10 +2,9 @@ import Profile from '@/components/Profile';
 import Header from '@/components/Header';
 import MyPageContent from '@/components/MyPageContent';
 
-export default function MyPage({}) {
+export default function MyPage() {
   return (
     <>
-      <Header pageName="마이페이지" />
       <div className="flex justify-center">
         <Profile />
       </div>
--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -2,6 +2,7 @@
 
 import { FormEvent } from 'react';
 import { useRouter } from 'next/navigation';
+import SearchEnterSVG from '@/components/(SVG_component)/SearchEnter';
 
 export default function SearchBar() {
   const router = useRouter();
@@ -19,23 +20,7 @@ export default function SearchBar() {
           <input name="search_input" type="search" id="search_input" placeholder="검색어를 입력해주세요." required />
         </div>
         <button className="mr-3">
-          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="16" cy="16" r="16" fill="url(#paint0_linear_222_444)" />
-            <circle cx="16" cy="16" r="5" fill="white" />
-            <defs>
-              <linearGradient
-                id="paint0_linear_222_444"
-                x1="24"
-                y1="26.5"
-                x2="16"
-                y2="16"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#007AFF" />
-                <stop offset="1" stopColor="#3897F0" />
-              </linearGradient>
-            </defs>
-          </svg>
+          <SearchEnterSVG />
         </button>
       </form>
     </section>
