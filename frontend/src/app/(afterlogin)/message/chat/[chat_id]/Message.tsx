71a3c9716267b4ab13cc5c5da63a9a635e377576
이주영 <git.leej.young@gmail.com>
2024-09-09 14:51:43 +0900

    feat: 채팅방 메시지 목록 레이아웃 및 더미 데이터 삽입

--- frontend/src/app/(afterlogin)/message/chat/[chat_id]/Message.tsx
+++ frontend/src/app/(afterlogin)/message/chat/[chat_id]/Message.tsx
@@ -1,27 +1,26 @@
 type LentInfo = {
-    title: string;
-    period: string;
-  };
-  
-  type UserInfo = {
-    // 유저 정보
-  };
-  
-  type Message = {
-    type: number;
-    user: UserInfo;
-    body?: string;
-    info?: LentInfo;
-    timestamp: string;
-  };
-  
-  export default function Message({
-    type,
-    user,
-    body,
-    info,
-    timestamp,
-  }: Message) {
-    // 메시지 타입에 따라 처리 분리
-  }
-  
+  title: string;
+  period: string;
+};
+
+type UserInfo = {
+  // 유저 정보
+};
+
+type Message = {
+  type: number;
+  user: UserInfo;
+  body?: string;
+  info?: LentInfo;
+  timestamp: string;
+};
+
+export default function Message({
+  type,
+  user,
+  body,
+  info,
+  timestamp,
+}: Message) {
+  // 메시지 타입에 따라 처리 분리
+}
--- frontend/src/app/(afterlogin)/message/chat/[chat_id]/page.tsx
+++ frontend/src/app/(afterlogin)/message/chat/[chat_id]/page.tsx
@@ -1,14 +1,18 @@
 'use client';
 
-import Header from '@/app/(afterlogin)/_component/Header';
 import { usePathname } from 'next/navigation';
+// eslint-disable-next-line import/extensions
+import Header from '@/app/(afterlogin)/_component/Header';
 
 export default function Chat() {
-  const pathname = usePathname().substring('/message/chat/'.length);
+  const pathname = decodeURI(usePathname().substring('/message/chat/'.length));
   // 채팅 참여 상대방 닉네임 떼어서 저장
   const roomName: string = `${pathname}님과의 채팅`;
 
   return (
-    <Header pageName={roomName} hasPrevBtn hasSearchBtn={false} hasAlertBtn />
+    <Header pageName={roomName} hasPrevBtn hasSearchBtn hasAlertBtn />
+
+  // 메시지 타입이나 DB 구조에 따라 다르지만 ..
+
   );
 }
