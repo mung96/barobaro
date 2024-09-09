2a29c65d9d059e2e5b2a6c782c16bdc2442fb321
이주영 <git.leej.young@gmail.com>
2024-09-09 15:41:41 +0900

    refactor : URL 닉네임 변수 받아오는 로직 수정

--- frontend/src/app/(afterlogin)/message/chat/[chat_id]/page.tsx
+++ frontend/src/app/(afterlogin)/message/chat/[chat_id]/page.tsx
@@ -1,17 +1,16 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
+import { useParams } from 'next/navigation';
 // eslint-disable-next-line import/extensions
 import Header from '@/components/Header';
 
 export default function Chat() {
-  const pathname = decodeURI(usePathname().substring('/message/chat/'.length));
   // 채팅 참여 상대방 닉네임 떼어서 저장
-  const roomName: string = `${pathname}님과의 채팅`;
-
+  const { chat_id: chatId } = useParams();
+  const roomName: string = `${typeof chatId === 'string' && decodeURI(chatId)}님과의 채팅`;
   return (
-    <Header pageName={roomName} hasPrevBtn hasSearchBtn hasAlertBtn />
-
-    // 메시지 타입이나 DB 구조에 따라 다르지만 ..
+    <>
+      <Header pageName={roomName} hasPrevBtn hasSearchBtn hasAlertBtn />
+    </>
   );
 }
