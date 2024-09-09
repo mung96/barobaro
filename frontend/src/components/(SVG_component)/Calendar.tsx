b83eba85c9aa3989a9c2213a113f81d87fb132b9
한지웅 <wldnd2977@gmail.com>
2024-09-09 17:54:55 +0900

    design : mypage css

---
+++ frontend/src/components/(SVG_component)/Calendar.tsx
@@ -0,0 +1,13 @@
+export default function CalendarSVG() {
+  return (
+    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="1.75" y="3.5" width="10.5" height="8.75" rx="1" stroke="#747483" strokeWidth="0.5" />
+      <path
+        d="M1.55554 5.44466C1.55554 5.13505 1.55554 4.98024 1.57265 4.85028C1.69081 3.95281 2.39702 3.24659 3.29449 3.12844C3.42446 3.11133 3.57926 3.11133 3.88888 3.11133H10.1111C10.4207 3.11133 10.5755 3.11133 10.7055 3.12844C11.603 3.24659 12.3092 3.95281 12.4273 4.85028C12.4444 4.98024 12.4444 5.13505 12.4444 5.44466H1.55554Z"
+        fill="#747483"
+      />
+      <path d="M4.08334 1.75L4.08334 3.5" stroke="#747483" strokeWidth="0.5" strokeLinecap="round" />
+      <path d="M9.91666 1.75L9.91666 3.5" stroke="#747483" strokeWidth="0.5" strokeLinecap="round" />
+    </svg>
+  );
+}
