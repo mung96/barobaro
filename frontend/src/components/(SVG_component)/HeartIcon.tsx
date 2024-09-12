f22302a30b109e9e58ad21aecee89ffc73b5428f
한지웅 <wldnd2977@gmail.com>
2024-09-12 15:17:38 +0900

    design : HeartIcon stroke detail change

--- frontend/src/components/(SVG_component)/HeartIcon.tsx
+++ frontend/src/components/(SVG_component)/HeartIcon.tsx
@@ -7,14 +7,14 @@ export default function HeartIcon({ fill } : Props) {
     <svg width="16" height="15" viewBox="0 0 16 15" fill={fill} xmlns="http://www.w3.org/2000/svg">
       <path
         d="M4.5 1C2.5673 1 1 2.61538 1 4.60837C1 6.2172 1.6125 10.0355 7.6416 13.8954C7.7496 13.9638 7.87358 14 8 14C8.12642 14 8.2504 13.9638 8.3584 13.8954C14.3875 10.0355 15 6.2172 15 4.60837C15 2.61538 13.4327 1 11.5 1C9.5673 1 8 3.18689 8 3.18689C8 3.18689 6.4327 1 4.5 1Z"
-        stroke="#C6C6C6"
+        stroke={fill === 'none' ? '#C6C6C6' : 'none'}
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M4.5 1C2.5673 1 1 2.61538 1 4.60837C1 6.2172 1.6125 10.0355 7.6416 13.8954C7.7496 13.9638 7.87358 14 8 14C8.12642 14 8.2504 13.9638 8.3584 13.8954C14.3875 10.0355 15 6.2172 15 4.60837C15 2.61538 13.4327 1 11.5 1C9.5673 1 8 3.18689 8 3.18689C8 3.18689 6.4327 1 4.5 1Z"
-        stroke="white"
+        stroke={fill === 'none' ? 'white' : 'none'}
         strokeLinecap="round"
         strokeLinejoin="round"
       />
