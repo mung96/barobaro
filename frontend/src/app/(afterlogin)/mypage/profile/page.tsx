2320cb1f364dfc634ff303b4ebb1564282b07e98
한지웅 <wldnd2977@gmail.com>
2024-09-12 11:12:10 +0900

    profile setting css + profile upload & preview

--- frontend/src/app/(afterlogin)/mypage/profile/page.tsx
+++ frontend/src/app/(afterlogin)/mypage/profile/page.tsx
@@ -2,12 +2,43 @@
 
 import NavBarLayout from '@/layout/NavBarLayout';
 import CameraBody from '@/components/(SVG_component)/CameraBody';
+import { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
 
 export default function Profile() {
-  const profileSetting = () => {
-    console.log('profileSetting');
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleProfileImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      setImageFile(file);
+    } else {
+      setImageFile(null);
+    }
   };
-  // 현재 프로필 사진, 닉네임을 가져오고 이를 프로필 section에 나타낸다.
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const confirmProfileImage = () => {
+    console.log('confirm');
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(imageFile);
+    } else {
+      setPreview(null);
+    }
+  }, [imageFile]);
+
   return (
     <NavBarLayout current="mypage">
       <header className="font-bold text-[15px] flex flex-col items-center mt-3 mb-10">
@@ -15,16 +46,27 @@ export default function Profile() {
       </header>
       <main className="flex flex-col items-center h-[80dvh]">
         <section className="w-[90px] h-[90px] justify-center items-center relative">
-          <div className="bg-gray-500 w-[89px] h-[89px] rounded-full" />
+          <div className="bg-gray-500 w-[89px] h-[89px] rounded-full overflow-hidden relative">
+            {preview && (
+              <Image src={preview} alt="Profile preview" fill style={{ objectFit: 'cover' }} className="rounded-full" />
+            )}
+          </div>
           <button
             type="button"
-            onClick={() => profileSetting()}
             className="bg-gray-400 w-[25px] h-[25px] rounded-full z-10 absolute bottom-0 right-0"
+            onClick={handleButtonClick}
           >
             <div className="flex items-center justify-center">
               <CameraBody fill="#747483" width="15.2" height="12.67" />
             </div>
           </button>
+          <input
+            ref={fileInputRef}
+            type="file"
+            accept="image/*"
+            className="hidden"
+            onChange={handleProfileImage}
+          />
         </section>
         <section className="w-[84dvw]">
           <div className="mb-4 text-[13px]">
@@ -34,7 +76,8 @@ export default function Profile() {
             <input className="w-[82dvw]" />
           </div>
         </section>
-        <button type="button" className="bg-blue-100 w-[104px] h-[29px] rounded-[5px] absolute bottom-[14dvh]">
+
+        <button type="button" className="bg-blue-100 w-[104px] h-[29px] rounded-[5px] absolute bottom-[14dvh]" onClick={() => confirmProfileImage()}>
           <p className="font-bold text-[14px] text-white">변경</p>
         </button>
       </main>
