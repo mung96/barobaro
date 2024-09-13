060430e4e601ab40be79e4c0a8ef4f9724e1727d
이주영 <git.leej.young@gmail.com>
2024-09-13 15:55:45 +0900

    design : 채팅방 레이아웃 작업

--- frontend/src/components/message/chat/Message.tsx
+++ frontend/src/components/message/chat/Message.tsx
@@ -2,11 +2,13 @@
 //   title: string;
 //   period: string;
 // };
-//
+
+import React from 'react';
+
 // type UserInfo = {
 //   // 유저 정보
 // };
-//
+
 // type Message = {
 //   type: number;
 //   user: UserInfo;
@@ -15,9 +17,20 @@
 //   timestamp: string;
 // };
 
+const go: React.CSSProperties = {
+  maxWidth: '70vw',
+};
 export default function Message() {
   // 메시지 타입에 따라 처리 분리
   return (
-    <h1>..;.;</h1>
+    <div className="flex justify-end pr-[2vh] pl-[2vh]">
+      <div
+        className="flex whitespace-normal p-[2.3vh] text-xs overflow-hidden rounded-tl-2xl rounded-b-2xl text-white bg-blue-100 mb-[2vh]"
+        style={go}
+      >
+        왜안가는데왜안가는데왜안가는데왜안가는데왜안가는데왜안가는데왜안가는데
+        ㅋㅋㅋㅋㅋㅋㅋ ㅋㅋ
+      </div>
+    </div>
   );
 }
