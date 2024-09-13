25e812537400eecd3324c9b613d9acdb0692cee7
한지웅 <wldnd2977@gmail.com>
2024-09-13 14:58:30 +0900

    refactor : Header 미삽입 페이지 마무리

---
+++ frontend/src/app/(afterlogin)/mypage/help/question/page.tsx
@@ -0,0 +1,14 @@
+import NavBarLayout from '@/layout/NavBarLayout';
+import Header from '@/components/Header';
+
+export default function FAQ() {
+  return (
+    <NavBarLayout current="mypage">
+      <header>
+        <Header pageName="마이페이지" hasPrevBtn hasSearchBtn={false} hasAlertBtn />
+        <h1 className="text-[15px] font-bold text-center">자주 묻는 질문</h1>
+      </header>
+      <main>뭐야질문하지마요</main>
+    </NavBarLayout>
+  );
+}
