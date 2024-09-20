60ec3a596268e10d0d42d0229c27ff8a7cdc1559
한지웅 <wldnd2977@gmail.com>
2024-09-20 15:43:59 +0900

    feat : search category layout 설정 S11P21A401-353

--- frontend/src/app/(afterlogin)/search/category/[name]/page.tsx
+++ frontend/src/app/(afterlogin)/search/category/[name]/page.tsx
@@ -1,7 +1,13 @@
+import NavBarLayout from '@/layout/NavBarLayout';
+
 export default function CategoryDetail() {
   return (
-    <>
-      asdasdsd
-    </>
+      <NavBarLayout current="home">
+          <h1>정보가 주어지면</h1>
+          <h1>itemList 만든거 활용해서</h1>
+          <h1>이곳에 나열하도록 합니다</h1>
+          <div className="h-[200dvh] bg-amber-200 w-[50px]">
+          </div>
+      </NavBarLayout>
   );
 }
