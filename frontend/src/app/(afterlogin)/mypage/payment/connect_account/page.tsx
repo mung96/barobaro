89355dd379edfe6d5d4131ddea1974738782576e
한지웅 <wldnd2977@gmail.com>
2024-09-20 10:39:01 +0900

    refactor : 불필요한 tag 수정 S11P21A401-350

--- frontend/src/app/(afterlogin)/mypage/payment/connect_account/page.tsx
+++ frontend/src/app/(afterlogin)/mypage/payment/connect_account/page.tsx
@@ -4,9 +4,7 @@ import Header from '@/components/Header';
 export default function ConnectAccountPage() {
   return (
     <NavBarLayout current="mypage">
-      <header>
         <Header pageName="마이페이지" hasPrevBtn hasSearchBtn={false} hasAlertBtn />
-      </header>
       <main>
         <p>계좌연결</p>
       </main>
