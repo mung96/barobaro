502e05a0db11e1722c19b5969da0fee1f8100cd8
이주영 <git.leej.young@gmail.com>
2024-09-20 10:31:39 +0900

    chore : 22번 MR 머지 전 수정사항 반영 S11P21A401-268

--- frontend/src/components/message/chat/ChatWindow.tsx
+++ frontend/src/components/message/chat/ChatWindow.tsx
@@ -2,12 +2,11 @@
 
 import AttatchImage from '@/components/(SVG_component)/(message)/(chat)/AttatchImage';
 import SendButton from '@/components/(SVG_component)/(message)/(chat)/SendButton';
-import { ChangeEvent, useState, useRef } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 export default function ChatWindow() {
-  const [chatValue, setChatValue] = useState<string>('');
-  const [active, setActive] = useState<boolean>(false);
-  const chatWindowRef = useRef<HTMLDivElement>(null);
+  const [chatValue, setChatValue] = useState('');
+  const [active, setActive] = useState(false);
 
   const handleChatValue = (e: ChangeEvent<HTMLInputElement>) => {
     setChatValue(e.target.value);
@@ -16,7 +15,7 @@ export default function ChatWindow() {
 
   return (
     <div>
-      <div ref={chatWindowRef} className="w-[100vw] bg-white p-2">
+      <div className="w-[100vw] bg-white p-2">
         <div className="w-full h-[6vh] flex justify-center">
           <div className="w-[95%] h-full flex items-center justify-around rounded-2xl border border-gray-500 bg-gray-400">
             <AttatchImage className="w-2/12" />
@@ -26,7 +25,7 @@ export default function ChatWindow() {
               onChange={handleChatValue}
               value={chatValue}
             />
-            <SendButton active={active} className="w-2/12" />
+            <SendButton active={!!chatValue} className="w-2/12" />
           </div>
         </div>
       </div>
--- frontend/src/components/message/chat/Message.tsx
+++ frontend/src/components/message/chat/Message.tsx
@@ -17,19 +17,13 @@ import React from 'react';
 //   timestamp: string;
 // };
 
-const go: React.CSSProperties = {
-  maxWidth: '70vw',
-};
 export default function Message() {
   // 메시지 타입에 따라 처리 분리
   return (
     <div className="flex justify-end pr-[2vh] pl-[2vh]">
-      <div
-        className="flex whitespace-normal p-[2.3vh] text-xs overflow-hidden rounded-tl-2xl rounded-b-2xl text-white bg-blue-100 mb-[2vh]"
-        style={go}
-      >
-        왜안가는데왜안가는데왜안가는데왜안가는데왜안가는데왜안가는데왜안가는데
-        ㅋㅋㅋㅋㅋㅋㅋ ㅋㅋ
+      <div className="flex whitespace-normal p-[2.3vh] text-xs overflow-hidden rounded-tl-2xl rounded-b-2xl text-white bg-blue-100 mb-[2vh] max-w-[70vw]">
+        배고파요배고파요배고파요배고파요배고파요
+        배고파요배고파요배고파요배고파요배고파요배고파요배고파요배고파요
       </div>
     </div>
   );
