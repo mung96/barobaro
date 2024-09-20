27f4895cadd5921d7750c31c810009ff27764bd3
이주영 <git.leej.young@gmail.com>
2024-09-20 16:48:01 +0900

    design : 채팅방 메시지 디자인 초안 작업 S11P21A401-270

--- frontend/src/app/(afterlogin)/message/chat/[chat_id]/page.tsx
+++ frontend/src/app/(afterlogin)/message/chat/[chat_id]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useParams } from 'next/navigation';
+import { useEffect, useRef } from 'react';
 import Header from '@/components/Header';
 import OriginBoard from '@/components/message/chat/OriginBoard';
 import Dialogs from '@/components/message/chat/Dialogs';
@@ -10,6 +11,13 @@ export default function Chat() {
   // 채팅 참여 상대방 닉네임 떼어서 저장
   const { chat_id: chatId } = useParams();
   const roomName: string = `${typeof chatId === 'string' && decodeURI(chatId)}님과의 채팅`;
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }, []);
 
   return (
     <div className="flex flex-col h-screen">
@@ -20,7 +28,10 @@ export default function Chat() {
       </div>
 
       {/* 스크롤 가능한 Dialogs 영역 */}
-      <div className="flex-1 mt-[25vh] mb-[8vh] overflow-y-scroll">
+      <div
+        className="flex-1 mt-[25vh] pb-[8vh] overflow-y-scroll"
+        ref={scrollRef}
+      >
         <Dialogs />
       </div>
 
