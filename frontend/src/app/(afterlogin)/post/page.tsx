a9ec5ff60624e85840f622eb4475a87a45f4b40b
정지연 <jung_j_yeon@naver.com>
2024-09-20 11:22:19 +0900

    feat : frontend 자료 삭제 S11P21A401-360

--- frontend/src/app/(afterlogin)/post/page.tsx
+++
@@ -1,5 +0,0 @@
-import NavBarLayout from '@/layout/NavBarLayout';
-
-export default function Post() {
-  return <NavBarLayout current="post">Post</NavBarLayout>;
-}
--- frontend/src/components/message/ChatRoomType.tsx
+++
@@ -1,8 +0,0 @@
-export interface ChatRoomType {
-  profileImageSrc: string; // 상대방 프로필사진
-  otherUserNickname: string; // 상대방 닉네임
-  lastMessage: string; // 마지막으로 전송받은 메시지
-  stuffThumbImageSrc: string; // 거래 진행 중인 게시글 썸네일
-  unread: boolean;
-  own: boolean;
-}
