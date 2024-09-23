6b8ba84b16318122ab7f9a2e31020ded409f4b05
mung96 <mung5079@gmail.com>
2024-09-23 16:59:49 +0900

    feat: dropDown시 애니메이션 컴포넌트 구현 S11P21A401-344

---
+++ frontend/src/components/post/DropDownAnimation.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { motion, AnimatePresence } from 'framer-motion';
+
+type DropDownAnimationProps = {
+  children: React.ReactNode;
+  isOpen: boolean;
+};
+
+function DropDownAnimation({ children, isOpen }: DropDownAnimationProps) {
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={{ opacity: 0, y: -10, height: 0 }}
+        animate={
+          isOpen
+            ? { opacity: 1, y: 0, height: '376px' }
+            : { opacity: 0, y: -10, height: 0 }
+        }
+        exit={{ opacity: 1, y: -10, height: '110px' }}
+        transition={{ duration: 0.15 }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
+export default DropDownAnimation;
