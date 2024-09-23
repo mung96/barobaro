296a33fe975398584d0d0882f53b66567c8809f0
이윤지 <yunji383614@gmail.com>
2024-09-23 17:17:18 +0900

    feat : 성공 코드 추가 S11P21A401-391

--- backend/baro/baro/src/main/java/baro/baro/global/statuscode/SuccessCode.java
+++ backend/baro/baro/src/main/java/baro/baro/global/statuscode/SuccessCode.java
@@ -10,7 +10,8 @@ import static org.springframework.http.HttpStatus.OK;
 @AllArgsConstructor
 public enum SuccessCode {
     RENTAL_PRODUCT_LIST_OK(OK.value(), "빌린 내역 리스트 조회에 성공했습니다."),
-    OWNER_PRODUCT_LIST_OK(OK.value(), "빌려준 내역 리스트 조회에 성공했습니다.")
+    OWNER_PRODUCT_LIST_OK(OK.value(), "빌려준 내역 리스트 조회에 성공했습니다."),
+    ACCOUNT_LIST_OK(OK.value(), "계좌 리스트 조회에 성공했습니다.")
     ;
 
     private final int httpStatusCode;
