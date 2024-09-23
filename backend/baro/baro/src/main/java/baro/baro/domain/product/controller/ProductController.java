12aeadb8ac7a226b078fe880a601155fb77ae4bd
이윤지 <yunji383614@gmail.com>
2024-09-23 16:07:26 +0900

    feat : 빌려준 내역 리스트 조회 Mock 생성 S11P21A401-378

--- backend/baro/baro/src/main/java/baro/baro/domain/product/controller/ProductController.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/product/controller/ProductController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import static baro.baro.domain.product.entity.ProductStatus.FINISH;
 import static baro.baro.domain.product.entity.ProductStatus.IN_PROGRESS;
+import static baro.baro.global.statuscode.SuccessCode.OWNER_PRODUCT_LIST_OK;
 import static baro.baro.global.statuscode.SuccessCode.RENTAL_PRODUCT_LIST_OK;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -60,5 +61,46 @@ public class ProductController {
         ProductListRes result = new ProductListRes(products);
 
         return new ResponseEntity<>(ResponseDto.success(RENTAL_PRODUCT_LIST_OK, result), OK);
+    }
+
+    @GetMapping("/members/me/owner")
+    public ResponseEntity<?> ownerProductList() {
+        List<ProductDto> products = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++) {
+            Long id = 10000L + i;
+
+            ProductDto dto = ProductDto.builder()
+                    .product_id(id)
+                    .product_main_image("대표 이미지 " + id)
+                    .title("제목 " + id)
+                    .startDate(LocalDate.of(2024, 1, 2))
+                    .endDate(LocalDate.of(2024, 5, 24))
+                    .rentalFee(100000)
+                    .productStatus(IN_PROGRESS)
+                    .build();
+
+            products.add(dto);
+        }
+
+        for(int i = 5; i < 10; i++) {
+            Long id = 10000L + i;
+
+            ProductDto dto = ProductDto.builder()
+                    .product_id(id)
+                    .product_main_image("대표 이미지 " + id)
+                    .title("제목 " + id)
+                    .startDate(LocalDate.of(2024, 1, 2))
+                    .endDate(LocalDate.of(2024, 5, 24))
+                    .rentalFee(100000)
+                    .productStatus(FINISH)
+                    .build();
+
+            products.add(dto);
+        }
+
+        ProductListRes result = new ProductListRes(products);
+
+        return new ResponseEntity<>(ResponseDto.success(OWNER_PRODUCT_LIST_OK, result), OK);
     }
 }
