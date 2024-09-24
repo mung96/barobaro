573803e62df072cc9bb959382788905296359291
박동환 <srpark13@naver.com>
2024-09-24 15:00:22 +0900

    feat : 계약 요청 조회 Mock API 구현 S11P21A401-413

--- backend/baro/baro/src/main/java/baro/baro/domain/contract/controller/ContractController.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/contract/controller/ContractController.java
@@ -1,19 +1,19 @@
 package baro.baro.domain.contract.controller;
 
 import baro.baro.domain.contract.dto.ContractRequestDto;
+import baro.baro.domain.contract.dto.request.ContractRequestDetailReq;
 import baro.baro.domain.product.entity.ReturnType;
 import baro.baro.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 import static baro.baro.global.statuscode.SuccessCode.CONTRACT_REQUEST_CREATED;
+import static baro.baro.global.statuscode.SuccessCode.CONTRACT_REQUEST_OK;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,5 +31,18 @@ public class ContractController {
         );
 
         return new ResponseEntity<>(ResponseDto.success(CONTRACT_REQUEST_CREATED, result), CREATED);
+    }
+
+    @GetMapping("/request")
+    public ResponseEntity<?> contractRequestDetail(@RequestBody ContractRequestDetailReq contractRequestDetailReq){
+
+        ContractRequestDto result = new ContractRequestDto(
+                contractRequestDetailReq.getProductId(),
+                LocalDate.of(2024, 9, 25),
+                LocalDate.of(2024, 10, 1),
+                ReturnType.DELIVERY
+        );
+
+        return new ResponseEntity<>(ResponseDto.success(CONTRACT_REQUEST_OK, result), OK);
     }
 }
