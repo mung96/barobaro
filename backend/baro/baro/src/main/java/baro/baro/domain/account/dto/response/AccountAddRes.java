96165c47f9afa726c8813306ee689a1ec2bf292a
이윤지 <yunji383614@gmail.com>
2024-09-24 12:22:51 +0900

    feat : 계좌 연결 Dto 생성 S11P21A401-409

---
+++ backend/baro/baro/src/main/java/baro/baro/domain/account/dto/response/AccountAddRes.java
@@ -0,0 +1,10 @@
+package baro.baro.domain.account.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AccountAddRes {
+    private String accountNumber;
+}
