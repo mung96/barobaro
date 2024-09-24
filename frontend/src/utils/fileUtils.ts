5ff09778fbccbb37822d6b82d6545858bf92b3b3
mung96 <mung5079@gmail.com>
2024-09-24 14:14:25 +0900

    featL 파일 읽는 util 구현 S11P21A401-417

---
+++ frontend/src/utils/fileUtils.ts
@@ -0,0 +1,21 @@
+export const readFile = (file: File): Promise<string | ArrayBuffer | null> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = () => reject(reader.error);
+  });
+};
+
+export const convertFileListToArray = async (files: FileList) => {
+  const fileArr: Array<string | ArrayBuffer | null> = [];
+  //파일을 변환하는 과정
+  await Promise.all(Array.from(files).map(readFile))
+    .then((results) => {
+      results.forEach((result) => fileArr.push(result));
+    })
+    .catch((error) => {
+      console.error('파일을 읽는 중 에러가 발생했습니다.', error);
+    });
+  return fileArr;
+};
