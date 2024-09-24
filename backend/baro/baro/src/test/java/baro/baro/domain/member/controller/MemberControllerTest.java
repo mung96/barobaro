67101af8cf1cfcc45f5b00d0059ecc435c45b9a0
이윤지 <yunji383614@gmail.com>
2024-09-24 15:45:50 +0900

    test : 프로필 수정 테스트 생성 S11P21A401-412

--- backend/baro/baro/src/test/java/baro/baro/domain/member/controller/MemberControllerTest.java
+++ backend/baro/baro/src/test/java/baro/baro/domain/member/controller/MemberControllerTest.java
@@ -1,6 +1,7 @@
 package baro.baro.domain.member.controller;
 
 import baro.baro.domain.member.dto.request.PasswordModifyReq;
+import baro.baro.domain.member.dto.request.ProfileModifyReq;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.epages.restdocs.apispec.Schema;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -18,13 +20,11 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static baro.baro.global.ResponseFieldUtils.getCommonResponseFields;
-import static baro.baro.global.statuscode.SuccessCode.PASSWORD_MODIFY_OK;
-import static baro.baro.global.statuscode.SuccessCode.PROFILE_DETAILS_OK;
+import static baro.baro.global.statuscode.SuccessCode.*;
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -130,11 +130,69 @@ class MemberControllerTest {
                                                         .description("프로필 이미지"),
                                                 fieldWithPath("body.nickname").type(JsonFieldType.STRING)
                                                         .description("닉네임"),
+                                                fieldWithPath("body.phoneNumber").type(JsonFieldType.STRING)
+                                                        .description("핸드폰 번호"),
                                                 fieldWithPath("body.email").type(JsonFieldType.STRING)
                                                         .description("이메일")
                                         )
                                 )
                                 .responseSchema(Schema.schema("프로필 조회 Response"))
+                                .build()
+                        ))
+                );
+    }
+
+    @Test
+    public void 프로필_수정_성공() throws Exception {
+        // given
+        ObjectMapper mapper = new ObjectMapper();
+
+        ProfileModifyReq req = new ProfileModifyReq("수정할 닉네임");
+
+        MockMultipartFile dto = new MockMultipartFile("dto", "", "application/json", mapper.writeValueAsBytes(req));
+        MockMultipartFile file = new MockMultipartFile("file", "sample.jpg", "image/jpeg", "image/sample.jpg".getBytes());
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                multipart("/members/me/profile")
+                        .file(dto)
+                        .file(file)
+                        .header("Authorization", "Bearer " + jwtToken)
+                        .contentType("multipart/form-data")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+        );
+
+        // then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.header.httpStatusCode").value(PROFILE_MODIFY_OK.getHttpStatusCode()))
+                .andExpect(jsonPath("$.header.message").value(PROFILE_MODIFY_OK.getMessage()))
+                .andDo(document(
+                        "프로필 수정",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("Member API")
+                                .summary("프로필 수정 API")
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("JWT 토큰")
+                                )
+                                .responseFields(
+                                        getCommonResponseFields(
+                                                fieldWithPath("body.profileImage").type(JsonFieldType.STRING)
+                                                        .description("프로필 이미지"),
+                                                fieldWithPath("body.nickname").type(JsonFieldType.STRING)
+                                                        .description("닉네임"),
+                                                fieldWithPath("body.phoneNumber").type(JsonFieldType.STRING)
+                                                        .description("핸드폰 번호"),
+                                                fieldWithPath("body.email").type(JsonFieldType.STRING)
+                                                        .description("이메일")
+                                        )
+                                )
+                                .requestSchema(Schema.schema("프로필 수정 Request"))
+                                .responseSchema(Schema.schema("프로필 수정 Response"))
                                 .build()
                         ))
                 );
