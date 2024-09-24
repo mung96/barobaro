dffcc72722e029acf8039bf94a7c2b574096d815
이윤지 <yunji383614@gmail.com>
2024-09-24 10:30:08 +0900

    feat : 프로필 조회 Mock API 생성 S11P21A401-408

--- backend/baro/baro/src/main/java/baro/baro/domain/member/controller/MemberController.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/member/controller/MemberController.java
@@ -2,16 +2,15 @@ package baro.baro.domain.member.controller;
 
 import baro.baro.domain.member.dto.request.PasswordModifyReq;
 import baro.baro.domain.member.dto.response.PasswordModifyRes;
+import baro.baro.domain.member.dto.response.ProfileDetailsRes;
 import baro.baro.global.dto.ResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static baro.baro.global.statuscode.SuccessCode.PASSWORD_MODIFY_OK;
+import static baro.baro.global.statuscode.SuccessCode.PROFILE_DETAILS_OK;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -23,5 +22,12 @@ public class MemberController {
         PasswordModifyRes result = new PasswordModifyRes("654321");
 
         return new ResponseEntity<>(ResponseDto.success(PASSWORD_MODIFY_OK, result), OK);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> profileDetails() {
+        ProfileDetailsRes result = new ProfileDetailsRes("프로필 이미지", "닉네임", "ssafy@ssafy.com");
+
+        return new ResponseEntity<>(ResponseDto.success(PROFILE_DETAILS_OK, result), OK);
     }
 }
