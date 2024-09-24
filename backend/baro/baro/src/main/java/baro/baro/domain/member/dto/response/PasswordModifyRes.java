dc83735b3fe006cd42515aee8ea1da24b2252c18
이윤지 <yunji383614@gmail.com>
2024-09-24 09:56:36 +0900

    feat : PIN번호 변경 Dto 추가 S11P21A401-404

---
+++ backend/baro/baro/src/main/java/baro/baro/domain/member/dto/response/PasswordModifyRes.java
@@ -0,0 +1,10 @@
+package baro.baro.domain.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PasswordModifyRes {
+    private String password;
+}
