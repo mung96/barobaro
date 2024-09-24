b4351d2f610ae47bc554d44a0f7698c3f6b3230e
정지연 <jung_j_yeon@naver.com>
2024-09-24 14:02:54 +0900

    chore : 성공코드 주석 추가 S11P21A401-365

--- backend/baro/baro/src/main/java/baro/baro/global/statuscode/SuccessCode.java
+++ backend/baro/baro/src/main/java/baro/baro/global/statuscode/SuccessCode.java
@@ -9,22 +9,37 @@ import static org.springframework.http.HttpStatus.OK;
 @Getter
 @AllArgsConstructor
 public enum SuccessCode {
+    //대여 물품
     PRODUCT_CREATED(CREATED.value(), "대여 물품 등록에 성공했습니다"),
 
+    //계약
+    CONTRACT_REQUEST_CREATED(CREATED.value(), "계약 요청에 성공했습니다."),
+
+    //채팅
+
+    //검색
+
+    //본인인증
+
+    //알림 및 푸시
+    NOTIFICATION_LIST_OK(OK.value(), "알림 리스트 조회에 성공했습니다."),
+    
+    //찜
+    
+    //마이페이지
     RENTAL_PRODUCT_LIST_OK(OK.value(), "빌린 내역 리스트 조회에 성공했습니다."),
     OWNER_PRODUCT_LIST_OK(OK.value(), "빌려준 내역 리스트 조회에 성공했습니다."),
-
+    
     ACCOUNT_LIST_OK(OK.value(), "계좌 리스트 조회에 성공했습니다."),
     ACCOUNT_ADD_CREATED(CREATED.value(), "계좌 연결에 성공했습니다."),
     ACCOUNT_ADD_MAIN_OK(OK.value(), "대표 계좌 설정에 성공했습니다."),
-
-	NOTIFICATION_LIST_OK(OK.value(), "알림 리스트 조회에 성공했습니다."),
-
+    
     PASSWORD_MODIFY_OK(OK.value(), "PIN번호 변경에 성공했습니다."),
     PROFILE_DETAILS_OK(OK.value(), "프로필 조회에 성공했습니다."),
 
-    CONTRACT_REQUEST_CREATED(CREATED.value(), "계약 요청에 성공했습니다."),
-
+    //지역 설정
+    
+    //회원가입 및 로그인
     ;
 
 	private final int httpStatusCode;
