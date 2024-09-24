29da26c368a59082c65a10a411602aa18d58a159
이윤지 <yunji383614@gmail.com>
2024-09-24 10:37:15 +0900

    feat : ProfileDetailsRes에 toDto 메소드 추가 S11P21A401-408

--- backend/baro/baro/src/main/java/baro/baro/domain/member/dto/response/ProfileDetailsRes.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/member/dto/response/ProfileDetailsRes.java
@@ -2,9 +2,11 @@ package baro.baro.domain.member.dto.response;
 
 import baro.baro.domain.member.entity.Member;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class ProfileDetailsRes {
     private String profileImage;
@@ -13,9 +15,11 @@ public class ProfileDetailsRes {
 
     private String email;
 
-    public ProfileDetailsRes(Member member) {
-        this.profileImage = member.getProfileImage();
-        this.nickname = member.getNickname();
-        this.email = member.getEmail();
+    public static ProfileDetailsRes toDto(Member member) {
+        return ProfileDetailsRes.builder()
+                .profileImage(member.getProfileImage())
+                .nickname(member.getNickname())
+                .email(member.getEmail())
+                .build();
     }
 }
