83fe690d255473a6d7261611a2dee22356c2e2fb
한지웅 <wldnd2977@gmail.com>
2024-09-25 10:47:56 +0900

    fix : delete parallel and intercept (beforelogin) S11P21A401-419

--- frontend/src/app/(beforelogin)/layout.tsx
+++ frontend/src/app/(beforelogin)/layout.tsx
@@ -1,12 +1,7 @@
 import { ReactNode } from 'react';
 
-type Props = { children : ReactNode, modal : ReactNode };
+type Props = { children: ReactNode; modal: ReactNode };
 
-export default function Layout({ children, modal }: Props) {
-  return (
-    <div>
-      {children}
-      {modal}
-    </div>
-  );
+export default function Layout({ children }: Props) {
+  return <div>{children}</div>;
 }
