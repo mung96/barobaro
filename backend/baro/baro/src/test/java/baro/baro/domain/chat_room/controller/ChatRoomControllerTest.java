98832aa7179b136f37f04e0448ce08d2392539bc
이윤지 <yunji383614@gmail.com>
2024-09-25 10:27:07 +0900

    test : 채팅방 리스트 조회 테스트 생성 S11P21A401-428

---
+++ backend/baro/baro/src/test/java/baro/baro/domain/chat_room/controller/ChatRoomControllerTest.java
@@ -0,0 +1,91 @@
+package baro.baro.domain.chat_room.controller;
+
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.Schema;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static baro.baro.global.ResponseFieldUtils.getCommonResponseFields;
+import static baro.baro.global.statuscode.SuccessCode.CHATROOM_LIST_OK;
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Transactional
+class ChatRoomControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final static String jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+    @Test
+    public void 채팅방_리스트_조회_성공() throws Exception {
+        // given
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                get("/chatrooms")
+                        .header("Authorization", jwtToken)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.header.httpStatusCode").value(CHATROOM_LIST_OK.getHttpStatusCode()))
+                .andExpect(jsonPath("$.header.message").value(CHATROOM_LIST_OK.getMessage()))
+                .andDo(document(
+                        "채팅방 리스트 조회",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("ChatRoom API")
+                                .summary("채팅방 리스트 조회 API")
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("JWT 토큰")
+                                )
+                                .responseFields(
+                                        getCommonResponseFields(
+                                                fieldWithPath("body.*[].chatRoomId").type(JsonFieldType.NUMBER)
+                                                        .description("채팅방 아이디"),
+                                                fieldWithPath("body.*[].profileImage").type(JsonFieldType.STRING)
+                                                        .description("상대방 프로필 이미지"),
+                                                fieldWithPath("body.*[].nickname").type(JsonFieldType.STRING)
+                                                        .description("상대방 닉네임"),
+                                                fieldWithPath("body.*[].productMainImage").type(JsonFieldType.STRING)
+                                                        .description("대여 상품 대표 이미지"),
+                                                fieldWithPath("body.*[].lastChat").type(JsonFieldType.STRING)
+                                                        .description("마지막 채팅 메세지"),
+                                                fieldWithPath("body.*[].chatRoomStatus").type(JsonFieldType.STRING)
+                                                        .description("채팅방 상태")
+                                        )
+                                )
+                                .responseSchema(Schema.schema("채팅방 리스트 조회 Response"))
+                                .build()
+                        ))
+                );
+    }
+}
