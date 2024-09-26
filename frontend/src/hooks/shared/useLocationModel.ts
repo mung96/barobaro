d2c2468bdf7d598d1fd1cceacd2c2179a12404c8
mung96 <mung5079@gmail.com>
2024-09-26 10:26:05 +0900

    refactor: 변수명 변경 S11P21A401-437

--- frontend/src/hooks/shared/useLocationModel.ts
+++ frontend/src/hooks/shared/useLocationModel.ts
@@ -1,29 +1,29 @@
-import { getLocationByQuery } from '@/apis/shared/kakaoApi';
+import { getLocationListByQuery } from '@/apis/shared/kakaoApi';
 import { convertLocationDataToView } from '@/services/shared/location';
 import { Location } from '@/types/shared/location';
 
 import { useState } from 'react';
 
 const useLocationModel = () => {
-  const [locations, setLocations] = useState<Location[]>([]);
+  const [locationList, setLocationList] = useState<Location[]>([]);
 
-  const searchLocationByQuery = async (query: string) => {
+  const searchLocationListByQuery = async (query: string) => {
     try {
-      const locationData = await getLocationByQuery(query);
-      const locationViews: Location[] = [];
+      const locationListData = await getLocationListByQuery(query);
+      const locationListView: Location[] = [];
 
-      locationData.data.documents.map((location) => {
-        locationViews.push(convertLocationDataToView(location));
+      locationListData.data.documents.map((location) => {
+        locationListView.push(convertLocationDataToView(location));
       });
 
-      setLocations(locationViews);
+      setLocationList(locationListView);
     } catch (error) {
       //TODO: 에러 핸들링시 수정해야함
       alert('위치를 찾을 수 없습니다.');
     }
   };
 
-  return { locations, searchLocationByQuery };
+  return { locationList, searchLocationListByQuery };
 };
 
 export default useLocationModel;
