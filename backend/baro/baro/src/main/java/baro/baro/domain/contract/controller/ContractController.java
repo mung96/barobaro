dd26bf58b3cbf2a591986d3103c3794cdde37172
박동환 <srpark13@naver.com>
2024-09-26 14:44:58 +0900

    feat : 계약 요청 승인 Mock api 구현 S11P21A401-458

--- backend/baro/baro/src/main/java/baro/baro/domain/contract/controller/ContractController.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/contract/controller/ContractController.java
@@ -1,8 +1,10 @@
 package baro.baro.domain.contract.controller;
 
 import baro.baro.domain.contract.dto.ContractRequestDto;
+import baro.baro.domain.contract.dto.request.ContractApproveReq;
 import baro.baro.domain.contract.dto.request.ContractOptionDetailReq;
 import baro.baro.domain.contract.dto.request.ContractRequestDetailReq;
+import baro.baro.domain.contract.dto.response.ContractApproveRes;
 import baro.baro.domain.contract.dto.response.ContractOptionDetailRes;
 import baro.baro.domain.product.entity.ReturnType;
 import baro.baro.global.dto.ResponseDto;
@@ -16,8 +18,7 @@ import java.util.List;
 
 import static baro.baro.domain.product.entity.ReturnType.DELIVERY;
 import static baro.baro.domain.product.entity.ReturnType.DIRECT;
-import static baro.baro.global.statuscode.SuccessCode.CONTRACT_REQUEST_CREATED;
-import static baro.baro.global.statuscode.SuccessCode.CONTRACT_REQUEST_OK;
+import static baro.baro.global.statuscode.SuccessCode.*;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -40,10 +41,10 @@ public class ContractController {
     }
 
     @GetMapping("/request")
-    public ResponseEntity<?> contractRequestDetail(@RequestBody ContractRequestDetailReq contractRequestDetailReq){
+    public ResponseEntity<?> contractRequestDetail(@RequestBody ContractRequestDetailReq contractRequestDetailReq) {
 
         ContractRequestDto result = new ContractRequestDto(
-                contractRequestDetailReq.getProductId(),
+                contractRequestDetailReq.getChatRoomId(),
                 LocalDate.of(2024, 9, 25),
                 LocalDate.of(2024, 10, 1),
                 ReturnType.DELIVERY
@@ -53,7 +54,7 @@ public class ContractController {
     }
 
     @GetMapping("")
-    public ResponseEntity<?> contractOptionDetail(@RequestBody ContractOptionDetailReq contractOptionDetailReq){
+    public ResponseEntity<?> contractOptionDetail(@RequestBody ContractOptionDetailReq contractOptionDetailReq) {
 
         List<ReturnType> returnTypes = new ArrayList<>();
         returnTypes.add(DELIVERY);
@@ -69,4 +70,27 @@ public class ContractController {
                 .build();
         return new ResponseEntity<>(ResponseDto.success(CONTRACT_REQUEST_OK, result), OK);
     }
+
+    @PostMapping("/approve")
+    public ResponseEntity<?> approveContractRequest(@RequestBody ContractApproveReq contractApproveReq, @RequestParam(name = "type", defaultValue = "default") String type) {
+
+        ContractApproveRes result = switch (type) {
+            case "default" ->
+                //거래 status 변경 및 물품 status 변경
+                    ContractApproveRes.builder()
+                            .chatRoomId(contractApproveReq.getChatRoomId())
+                            .build();
+
+            case "contract" ->
+                //거래 status 변경 및 물품 status 변경
+                //계약서 Step1 진행 및 s3 업로드 후 url 반환
+                    ContractApproveRes.builder()
+                            .chatRoomId(contractApproveReq.getChatRoomId())
+                            .fileUrl("http://test.url/test.pdf")
+                            .build();
+            default -> null;
+        };
+        return new ResponseEntity<>(ResponseDto.success(CONTRACT_APPROVED_OK, result), OK);
+    }
+
 }
