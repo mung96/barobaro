722918c78e2185ac02d4903daa6f7bc131b9be49
정지연 <jung_j_yeon@naver.com>
2024-09-26 15:58:40 +0900

    feat : jwt 관련 실패 에러코드 추가 S11P21A401-461

--- backend/baro/baro/src/main/java/baro/baro/global/statuscode/ErrorCode.java
+++ backend/baro/baro/src/main/java/baro/baro/global/statuscode/ErrorCode.java
@@ -3,9 +3,15 @@ package baro.baro.global.statuscode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    //token
+    EXPIRED_TOKEN(BAD_REQUEST.value(), "만료된 토큰입니다."),
+    NOT_VALID_TOKEN(BAD_REQUEST.value(), "유효하지 않은 토큰입니다."),
+    ALREADY_EXIST_MEMBER(BAD_REQUEST.value(), "이미 가입된 유저입니다.")
     ;
 
     private final int httpStatusCode;
