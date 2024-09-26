5f19de548df858e492ff61a466c21ee27ac36895
정지연 <jung_j_yeon@naver.com>
2024-09-26 15:55:07 +0900

    feat : Cookie util 추가 S11P21A401-460

---
+++ backend/baro/baro/src/main/java/baro/baro/global/utils/CookieUtil.java
@@ -0,0 +1,22 @@
+package baro.baro.global.utils;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieUtil {
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie token = new Cookie(name, value);
+        token.setHttpOnly(true);
+        token.setSecure(true);
+        token.setAttribute("SameSite", "None");
+        token.setMaxAge(maxAge);
+
+        response.addCookie(token);
+    }
+
+    public static void deleteCookie(HttpServletResponse response, String name) {
+        Cookie cookie = new Cookie(name, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+}
