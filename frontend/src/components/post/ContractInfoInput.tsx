cad62d08154cfe9366df4cdb5fe3c34c858dc355
mung96 <mung5079@gmail.com>
2024-09-27 12:15:18 +0900

    refactor: DropDownAnimation 타이틀 부분 추상화 S11P21A401-464

--- frontend/src/components/post/ContractInfoInput.tsx
+++ frontend/src/components/post/ContractInfoInput.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/shared/Button';
+import DropDownAnimation from '@/components/shared/DropDownAnimation';
 import Radio from '@/components/shared/Radio';
 import SelectableItem from '@/components/shared/SelectableItem';
 import { CONTRACT_YN } from '@/constants/product';
@@ -14,7 +15,7 @@ type Props = {
 
 function ContractInfoInput({ onTotalStepChange, onNext, onPrev }: Props) {
   const [value, setValue] = useState('YES');
-
+  const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
     const totalStep = totalStepByContractYN(value);
     onTotalStepChange(totalStep);
@@ -22,7 +23,7 @@ function ContractInfoInput({ onTotalStepChange, onNext, onPrev }: Props) {
 
   return (
     <div className="flex flex-col gap-2">
-      <h2>전자계약서 작성 여부</h2>
+      <h2 onClick={() => setIsOpen(true)}>전자계약서 작성 여부</h2>
       <Radio.Group
         fieldSetName="전자계약서 작성 여부"
         value={value}
@@ -33,6 +34,11 @@ function ContractInfoInput({ onTotalStepChange, onNext, onPrev }: Props) {
           <SelectableItem type={'radio'} value={yn.value} label={yn.label} />
         ))}
       </Radio.Group>
+
+      <DropDownAnimation isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <div>물건 소유자 및 대여정보</div>
+      </DropDownAnimation>
+
       <div className="flex  gap-6">
         <Button onClick={onPrev} width="100%" height="36px" color="gray">
           <p className="text-xs">이전</p>
