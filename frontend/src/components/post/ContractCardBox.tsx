f30a0fbda5d2dccc042765e72ae70a36b6b6acc8
mung96 <mung5079@gmail.com>
2024-09-27 16:11:53 +0900

    feat: 계약 등록시 사용하는 카드 공통부분 추상화 S11P21A401-464

---
+++ frontend/src/components/post/ContractCardBox.tsx
@@ -0,0 +1,33 @@
+import DropDownAnimation from '@/components/shared/DropDownAnimation';
+import { useState } from 'react';
+import { IoIosArrowDown, IoIosArrowForward } from 'react-icons/io';
+
+type Props = {
+  title: string;
+  step: number;
+  children: React.ReactNode;
+};
+
+const ContractCardBox = ({ title, step, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div className="bg-gray-400 py-5 px-4 w-full rounded-xl flex flex-col gap-3">
+      <div
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="w-full flex justify-between"
+      >
+        <p className="text-base font-semibold">{title}</p>
+        <div className="flex items-center justify-center">
+          <p>
+            <span className="text-base font-bold">{step}</span>
+            <span className="text-base text-gray-300"> / 5</span>
+          </p>
+          {isOpen ? <IoIosArrowDown /> : <IoIosArrowForward />}
+        </div>
+      </div>
+      <DropDownAnimation isOpen={isOpen}>{children}</DropDownAnimation>
+    </div>
+  );
+};
+
+export default ContractCardBox;
