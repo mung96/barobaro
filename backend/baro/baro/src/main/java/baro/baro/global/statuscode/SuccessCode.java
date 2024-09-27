77a70b6ad3eb1187721aa8535b5bb62113c75cfe
박동환 <srpark13@naver.com>
2024-09-27 14:47:17 +0900

    feat : 계약물품 영상업로드 성공 응답코드 추가 S11P21A401-471

--- backend/baro/baro/src/main/java/baro/baro/global/statuscode/SuccessCode.java
+++ backend/baro/baro/src/main/java/baro/baro/global/statuscode/SuccessCode.java
@@ -23,7 +23,7 @@ public enum SuccessCode {
     CONTRACT_APPROVED_OK(OK.value(),"계약 요청이 승인되었습니다."),
     CONTRACT_SIGNED_OK(OK.value(),"계약 서명에 성공했습니다."),
     CONTRACT_TERMINATED_OK(OK.value(), "물품 회수가 확인되었습니다."),
-
+    PRODUCT_VIDEO_UPLOADED_OK(OK.value(), "영상이 정상 제출되었습니다."),
     //채팅
     CHATROOM_LIST_OK(OK.value(), "채팅방 리스트 조회에 성공했습니다."),
 
