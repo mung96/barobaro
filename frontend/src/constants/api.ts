6cedfb1cac60cbc3006732a495f3f6066323199f
mung96 <mung5079@gmail.com>
2024-09-27 13:16:04 +0900

    feat: api constant baseURL지정 S11P21A401-470

---
+++ frontend/src/constants/api.ts
@@ -0,0 +1,14 @@
+export const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
+export const SERVER_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const KAKAO_BASE_URL = 'https://dapi.kakao.com/v2/';
+
+export const END_POINT = {
+  LOCATION: 'local/search/keyword.json',
+  GOOGLE_LOGIN: 'members/signin/social/google',
+};
+
+export const NETWORK = {
+  RETRY_COUNT: 2,
+  TIMEOUT: 10000,
+};
