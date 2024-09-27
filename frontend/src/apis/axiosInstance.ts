85a0b2d71ba6ef326c10100f26b419cb9e7569b7
mung96 <mung5079@gmail.com>
2024-09-27 13:14:58 +0900

    feat: axios에 timeout 설정 S11P21A401-470

--- frontend/src/apis/axiosInstance.ts
+++ frontend/src/apis/axiosInstance.ts
@@ -1,17 +1,18 @@
 import {
   KAKAO_BASE_URL,
-  KAKO_REST_API_KEY,
+  KAKAO_REST_API_KEY,
+  NETWORK,
   SERVER_BASE_URL,
 } from '@/constants/api';
 import axios from 'axios';
 
 export const kakaoInstance = axios.create({
   baseURL: KAKAO_BASE_URL,
-  headers: { Authorization: `KakaoAK ${KAKO_REST_API_KEY}` },
+  headers: { Authorization: `KakaoAK ${KAKAO_REST_API_KEY}` },
 });
 
-export const axiosInstance = axios.create({
-  baseURL: SERVER_BASE_URL,
-
-  withCredentials: true,
+export const axiosinstance = axios.create({
+  baseURL: SERVER_BASE_URL, // 기본 URL
+  withCredentials: true, //도메인이 다른 서버에 쿠키 요청시 필요
+  timeout: NETWORK.TIMEOUT, //TimeOut 시간이 지나도 응답이 안오면 에러처리됨
 });
