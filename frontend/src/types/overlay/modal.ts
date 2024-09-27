994f858e008f112b32db4784da05778216cc2494
한지웅 <wldnd2977@gmail.com>
2024-09-27 16:32:26 +0900

    refactor : modal content S11P21A401-453

---
+++ frontend/src/types/overlay/modal.ts
@@ -0,0 +1,11 @@
+type ModalType =
+  | 'isComplete'
+  | 'noPermissionEdit'
+  | 'noPermissionDelete'
+  | 'needPassword';
+
+export type modalProps = {
+  modalType: ModalType;
+  isOpen: boolean;
+  onRequestClose: () => void;
+};
