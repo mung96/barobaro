2bfacfafd60f669842ded5d92223426fff3cb7df
mung96 <mung5079@gmail.com>
2024-09-27 12:41:13 +0900

    feat: axiosInstance 설정 S11P21A401-470

--- frontend/src/apis/axiosInstance.ts
+++ frontend/src/apis/axiosInstance.ts
@@ -1,7 +1,17 @@
-import { KAKAO_BASE_URL, KAKO_REST_API_KEY } from '@/apis/constants';
+import {
+  KAKAO_BASE_URL,
+  KAKO_REST_API_KEY,
+  SERVER_BASE_URL,
+} from '@/constants/api';
 import axios from 'axios';
 
 export const kakaoInstance = axios.create({
   baseURL: KAKAO_BASE_URL,
   headers: { Authorization: `KakaoAK ${KAKO_REST_API_KEY}` },
+});
+
+export const axiosInstance = axios.create({
+  baseURL: SERVER_BASE_URL,
+
+  withCredentials: true,
 });
