811fd0d0960bbab99cacb345a64c76e9de268616
mung96 <mung5079@gmail.com>
2024-09-27 13:21:56 +0900

    feat: api 관련 상수 추가 S11P21A401-470

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -6,9 +6,25 @@ export const KAKAO_BASE_URL = 'https://dapi.kakao.com/v2/';
 export const END_POINT = {
   LOCATION: 'local/search/keyword.json',
   GOOGLE_LOGIN: 'members/signin/social/google',
-};
+} as const;
+
+export const HTTP_STATUS_CODE = {
+  SUCCESS: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  FORBBIDEN: 409,
+  CONTENT_TOO_LARGE: 413,
+
+  INTERNAL_SERVER_ERROR: 500,
+} as const;
+
+export const ERROR_MESSAGE = {};
 
 export const NETWORK = {
   RETRY_COUNT: 2,
   TIMEOUT: 10000,
-};
+} as const;
