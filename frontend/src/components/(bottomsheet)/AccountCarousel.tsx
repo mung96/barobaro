79de4a13e9f21501e4899c1a63a949969529b73f
한지웅 <wldnd2977@gmail.com>
2024-09-27 13:01:27 +0900

    refactor : AccountCarousel Code 간소화 S11P21A401-453

--- frontend/src/components/(bottomsheet)/AccountCarousel.tsx
+++ frontend/src/components/(bottomsheet)/AccountCarousel.tsx
@@ -1,87 +1,42 @@
-'use client';
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
-import React, { useState, useEffect } from 'react';
-import AccountCard from '@/components/(bottomsheet)/AccountCard';
-import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import type { CSSProperties } from 'react';
+
+// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
-export default function AccountCarousel() {
-  const [width, setWidth] = useState(window.innerWidth > 500 ? 500 : window.innerWidth);
-  const handleWidthChange = () => {
-    setWidth(window.innerWidth > 500 ? 500 : window.innerWidth);
-  };
-  useEffect(() => {
-    window.addEventListener('resize', handleWidthChange);
-    return () => {
-      window.removeEventListener('resize', handleWidthChange);
-    };
-  }, []);
-
-  const accountList = [{
-    account_number: 1,
-    isSelected: false,
-    bank: '국민',
-    account_name: '나라사랑',
-  },
-  {
-    account_number: 2,
-    isSelected: false,
-    bank: '토스',
-    account_name: '토스',
-  },
-  {
-    account_number: 3,
-    isSelected: true,
-    bank: '신한',
-    account_name: 'SOL',
-  },
-  {
-    account_number: 4,
-    isSelected: false,
-    bank: '카카오',
-    account_name: '카카오',
-  },
-  ];
-
-  const swiperStyle: CSSProperties = {
-    paddingBottom: '30px',
-  };
-
-  const swiperCustomStyles = {
-    '--swiper-pagination-bullet-inactive-color': '#999999',
-    '--swiper-pagination-bullet-inactive-opacity': '1',
-    '--swiper-pagination-bullet-size': '8px',
-  } as Record<string, string>;
-
+export default () => {
   return (
     <Swiper
-      spaceBetween={10}
-      slidesPerView={1.5}
-      centeredSlides
-      initialSlide={0}
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      // navigation
+      centeredSlides={true}
       pagination={{ clickable: true }}
-      modules={[Pagination]}
-      style={{ ...swiperStyle, ...swiperCustomStyles }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      style={{ display: 'flex', alignItems: 'center' }}
     >
-      {accountList.map((item) => (
-        <SwiperSlide key={item.account_number}>
-          <AccountCard
-            width={width}
-            isSelected={item.isSelected}
-            bank={item.bank}
-            account_name={item.account_name}
-          />
-        </SwiperSlide>
-      ))}
-      <SwiperSlide key={accountList.length}>
-        <div className="bg-gray-300 m-4 flex flex-col rounded-[10px] justify-center items-center" style={{ width: `${width * 0.6}px`, height: `${width * 0.35}px` }}>
-          <button type="button">추가하기</button>
-        </div>
+      <SwiperSlide>
+        <div className="w-[300px] h-[200px] bg-amber-200 mx-auto"></div>
       </SwiperSlide>
+      <SwiperSlide>
+        <div className="w-[300px] h-[200px] bg-amber-200 mx-auto"></div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="w-[300px] h-[200px] bg-amber-200 mx-auto"></div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="w-[300px] h-[200px] bg-amber-200 mx-auto"></div>
+      </SwiperSlide>
+      ...
     </Swiper>
   );
-}
+};
--- frontend/src/components/(bottomsheet)/ConnectAccountContent.tsx
+++ frontend/src/components/(bottomsheet)/ConnectAccountContent.tsx
@@ -2,8 +2,8 @@ import AccountCarousel from '@/components/(bottomsheet)/AccountCarousel';
 
 export default function ConnectAccountContent() {
   return (
-    <section className="flex flex-col">
-      <section className="flex-grow flex flex-col justify-center my-3">
+    <section className="flex flex-col h-[360px]">
+      <section className="mb-3">
         <div className="flex flex-row justify-center">
           <h2 className="text-xl font-bold">주 거래계좌를 선택해주세요.</h2>
           <button type="button" className="absolute right-0 mr-3 text-blue-500">
@@ -11,13 +11,23 @@ export default function ConnectAccountContent() {
           </button>
         </div>
       </section>
-      <section className="flex-grow-[3] flex items-center my-3">
+      <div className="flex flex-1">
         <AccountCarousel />
-      </section>
-      <section className="flex-grow flex items-center">
-        <div className="w-full flex flex-row justify-evenly">
-          <button type="button" className="bg-gray-500 w-[40%] h-[50px] rounded-[10px] text-white">취소</button>
-          <button type="button" className="bg-blue-500 w-[40%] h-[50px] rounded-[10px] text-white">적용</button>
+      </div>
+      <section className="flex">
+        <div className="w-full flex justify-evenly">
+          <button
+            type="button"
+            className="bg-gray-500 w-[40%] h-[50px] rounded-[10px] text-white"
+          >
+            취소
+          </button>
+          <button
+            type="button"
+            className="bg-blue-500 w-[40%] h-[50px] rounded-[10px] text-white"
+          >
+            적용
+          </button>
         </div>
       </section>
     </section>
