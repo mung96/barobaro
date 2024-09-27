e5c4aa6490d01fc185c9ecc2a5d088ff6392ea96
한지웅 <wldnd2977@gmail.com>
2024-09-27 16:03:27 +0900

    refactor : itemList type API 명세에 맞게 설정, products.ts파일 활용 S11P21A401-453

---
+++ frontend/src/types/products/products.ts
@@ -0,0 +1,9 @@
+export type itemList = {
+  productId: number;
+  productMainImage: string;
+  title: string;
+  startDate: string;
+  endDate: string;
+  rentalFee: number;
+  productStatus: string;
+}[];
