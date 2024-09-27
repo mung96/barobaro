ef59ccd8446fbc2c8258005b3f6ee75cdfccf43e
한지웅 <wldnd2977@gmail.com>
2024-09-27 13:01:27 +0900

    refactor : AccountCarousel Code 간소화 S11P21A401-453

--- frontend/src/components/(bottomsheet)/AccountCard.tsx
+++ frontend/src/components/(bottomsheet)/AccountCard.tsx
@@ -1,27 +1,29 @@
 type Props = {
-  isSelected : boolean;
-  bank : string;
+  isSelected: boolean;
+  bank: string;
   account_name: string;
-  width : number;
+  width: number;
 };
 
 export default function AccountCard({
-  isSelected, bank, account_name, width,
-} : Props) {
+  isSelected,
+  bank,
+  account_name,
+  width,
+}: Props) {
   return (
-    <div className="bg-amber-200 m-4 flex flex-col rounded-[10px]" style={{ width: `${width * 0.6}px`, height: `${width * 0.35}px` }}>
+    <div
+      className="bg-amber-200 m-4 flex flex-col rounded-[10px]"
+      style={{ width: `${width * 0.6}px`, height: `${width * 0.35}px` }}
+    >
       <div className="flex flex-row justify-end m-3">
         {isSelected === true ? '현재 선택된 계좌입니다.' : null}
         {bank}
       </div>
       <div className="flex flex-1" />
       <div className="m-4">
-        <div>
-          {bank}
-        </div>
-        <div>
-          {account_name}
-        </div>
+        <div>{bank}</div>
+        <div>{account_name}</div>
       </div>
     </div>
   );
