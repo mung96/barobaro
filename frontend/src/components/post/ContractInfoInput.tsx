cd504474d0e24109d1186c9d2385f95c5f661380
mung96 <mung5079@gmail.com>
2024-09-27 10:05:55 +0900

    feat: 계약성 작성 여부 체크 컴포넌트 구현 S11P21A401-464

--- frontend/src/components/post/ContractInfoInput.tsx
+++ frontend/src/components/post/ContractInfoInput.tsx
@@ -1,15 +1,33 @@
 import Button from '@/components/shared/Button';
+import Radio from '@/components/shared/Radio';
+import SelectableItem from '@/components/shared/SelectableItem';
+import { CONTRACT_YN } from '@/constants/product';
 import { ContractConditionRequest } from '@/types/apis/productRequest';
+import { useState } from 'react';
 
 type Props = {
   onNext: (data: ContractConditionRequest) => void;
   onPrev: () => void;
 };
-
+const RETURN_TYPE = [
+  { label: '미작성', value: 'DIRECT' },
+  { label: '작성', value: 'DELIVERY' },
+];
 function ContractInfoInput({ onNext, onPrev }: Props) {
+  const [value, setValue] = useState('');
   return (
     <div className="flex flex-col">
-      <h2>계약서</h2>
+      <h2>전자계약서 작성 여부</h2>
+      <Radio.Group
+        fieldSetName="전자계약서 작성 여부"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="flex gap-4"
+      >
+        {CONTRACT_YN.map((yn) => (
+          <SelectableItem type={'radio'} value={yn.value} label={yn.label} />
+        ))}
+      </Radio.Group>
       <div className="flex  gap-6">
         <Button onClick={onPrev} width="100%" height="36px" color="gray">
           <p className="text-xs">이전</p>
