4c955f1f2c47a2b61a1bb8744efc4de3b494c37c
정지연 <jung_j_yeon@naver.com>
2024-09-30 13:20:55 +0900

    feat : 에러 코드 추가 S11P21A401-473

--- backend/baro/baro/src/main/java/baro/baro/global/statuscode/ErrorCode.java
+++ backend/baro/baro/src/main/java/baro/baro/global/statuscode/ErrorCode.java
@@ -4,14 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    //member
+    INVALID_NICKNAME(BAD_REQUEST.value(), "닉네임은 10자 이내의 영어 대소문자, 숫자, 한글만 사용해야합니다."),
+
     //token
     EXPIRED_TOKEN(BAD_REQUEST.value(), "만료된 토큰입니다."),
     NOT_VALID_TOKEN(BAD_REQUEST.value(), "유효하지 않은 토큰입니다."),
-    ALREADY_EXIST_MEMBER(BAD_REQUEST.value(), "이미 가입된 유저입니다.")
+    ALREADY_EXIST_MEMBER(BAD_REQUEST.value(), "이미 가입된 유저입니다."),
+
+    //file
+    FILE_UPLOAD_FAIL(BAD_REQUEST.value(), "파일 업로드에 실패했습니다."),
+    FILE_DELETE_FAIL(BAD_REQUEST.value(), "파일 삭제에 실패했습니다."),
+    FILE_EXTENSION_FAIL(BAD_REQUEST.value(), "파일 확장자가 올바르지 않습니다."),
+
+    //location
+    LOCATION_IS_EMPTY(BAD_REQUEST.value(), "지역은 최소 1개 이상을 선택해야합니다."),
+    INVALID_LOCATION_SIZE(BAD_REQUEST.value(), "지역은 최대 3개까지 선택가능합니다."),
+    LOCATION_NOT_FOUND(NOT_FOUND.value(), "지역을 찾을 수 없습니다."),
     ;
 
     private final int httpStatusCode;
