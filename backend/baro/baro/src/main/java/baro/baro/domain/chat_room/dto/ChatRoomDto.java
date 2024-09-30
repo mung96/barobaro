e0a1e396fb9bce0e92e03b8c2c4170e51ba79421
이윤지 <yunji383614@gmail.com>
2024-09-30 15:57:46 +0900

    feat : 채팅방 상세 정보 Dto 수정 S11P21A401-507

--- backend/baro/baro/src/main/java/baro/baro/domain/chat_room/dto/ChatRoomDto.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/chat_room/dto/ChatRoomDto.java
@@ -1,10 +1,16 @@
 package baro.baro.domain.chat_room.dto;
 
+import baro.baro.domain.chat_room.entity.ChatRoom;
+import baro.baro.domain.member.entity.Member;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
+@AllArgsConstructor
 public class ChatRoomDto {
     private Long chatRoomId;
 
@@ -16,5 +22,19 @@ public class ChatRoomDto {
 
     private String lastChat;
 
+    private LocalDateTime lastChatTime;
+
     private ChatRoomStatus chatRoomStatus;
+
+    public static ChatRoomDto toDto(ChatRoom chatRoom, Member member, String productMainImage, ChatRoomStatus chatRoomStatus) {
+        return ChatRoomDto.builder()
+                .chatRoomId(chatRoom.getId())
+                .profileImage(member.getProfileImage())
+                .nickname(member.getNickname())
+                .productMainImage(productMainImage)
+                .lastChat(chatRoom.getLastChat())
+                .lastChatTime(chatRoom.getLastChatTime())
+                .chatRoomStatus(chatRoomStatus)
+                .build();
+    }
 }
