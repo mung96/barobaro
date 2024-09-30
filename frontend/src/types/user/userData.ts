ca0637d3058c222dc37990b3f5273acbc5d01480
한지웅 <wldnd2977@gmail.com>
2024-09-30 10:45:23 +0900

    feat : user profile type S11P21A401-485

--- frontend/src/types/user/userData.ts
+++ frontend/src/types/user/userData.ts
@@ -2,3 +2,11 @@ export type UserDataPassword = {
   needNewPassword: boolean;
   password: string;
 };
+
+export type UserProfile = {
+  profileImage: string;
+  nickname: string;
+  phoneNumber: string;
+  email: string;
+  name: string;
+};
