74059f2c5808718cd3267d2bc6795658f53a0317
이윤지 <yunji383614@gmail.com>
2024-09-30 15:45:12 +0900

    feat : 대여 상품 이미지 Entity 생성 S11P21A401-507

---
+++ backend/baro/baro/src/main/java/baro/baro/domain/product_image/entity/ProductImage.java
@@ -0,0 +1,33 @@
+package baro.baro.domain.product_image.entity;
+
+import baro.baro.domain.member.entity.Member;
+import baro.baro.domain.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_image_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String imageKeyName;
+
+    private boolean main;
+}
