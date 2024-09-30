a90a6169e7ebaec56cea58749eb9b2bbc6d72df7
mung96 <mung5079@gmail.com>
2024-09-30 09:57:08 +0900

    feat: 청구 비용 컴포넌트 구현 S11P21A401-464

--- frontend/src/components/post/RefundInfoCard.tsx
+++ frontend/src/components/post/RefundInfoCard.tsx
@@ -1,9 +1,12 @@
 import ContractCardBox from '@/components/post/ContractCardBox';
+import { ContractWidget } from '@/components/post/ContractWidget';
+import { useState } from 'react';
 
 const RefundInfoCard = () => {
+  const [value,setValue] = useState('0');
   return (
     <ContractCardBox title={'청구 비용'} step={4}>
-      <div>청구 비용</div>
+     <ContractWidget  title='청구 비용 지급' name='청구한 시점 기준' value={value} end='일' />
     </ContractCardBox>
   );
 };
