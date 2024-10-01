0ba11a06279ff5cbe69750850ae9029dab8a1c31
박동환 <srpark13@naver.com>
2024-10-01 22:28:20 +0900

    feat : 계약 요청 조회 서비스 로직 작성 S11P21A401-517

--- backend/baro/baro/src/main/java/baro/baro/domain/contract/service/ContractService.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/contract/service/ContractService.java
@@ -1,9 +1,11 @@
 package baro.baro.domain.contract.service;
 
 import baro.baro.domain.contract.dto.ContractRequestDto;
+import baro.baro.domain.contract.dto.request.ContractRequestDetailReq;
 
 public interface ContractService {
 
     void addContractRequest(ContractRequestDto contractRequestDto,Long rentalId);
 
+    ContractRequestDto findContractRequestDetail(ContractRequestDetailReq contractRequestDetailReq, Long ownerId);
 }
