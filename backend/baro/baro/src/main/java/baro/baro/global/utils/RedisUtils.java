42230ef15cadabd2a7d9ab1288d091f78bba974c
박동환 <srpark13@naver.com>
2024-10-01 14:56:54 +0900

    feat : 레디스 lock 추가 및 인터페이스화 S11P21A401-512

--- backend/baro/baro/src/main/java/baro/baro/global/utils/RedisUtils.java
+++ backend/baro/baro/src/main/java/baro/baro/global/utils/RedisUtils.java
@@ -1,34 +1,27 @@
 package baro.baro.global.utils;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
-
+import java.util.List;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
-@Component
-@RequiredArgsConstructor
-public class RedisUtils {
-    private final RedisTemplate<String, Object> redisTemplate;
+public interface RedisUtils {
 
-    public void setData(String key, Object value) {
-        redisTemplate.opsForValue().set(key, value);
-    }
+    void setData(String key, Object value);
 
-    public void setDataWithExpiration(String key, Object value, Long expiredTime) {
-        redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.SECONDS);
-    }
+    void setDataWithExpiration(String key, Object value, Long expiredTime);
 
-    public Object getData(String key) {
-        return redisTemplate.opsForValue().get(key);
-    }
+    Object getData(String key);
 
-    public void deleteData(String key) {
-        redisTemplate.delete(key);
-    }
+    void addListData(String key, Object value);
 
-    public Set<String> keys(String pattern) {
-        return redisTemplate.keys(pattern);
-    }
+    List<Object> getListData(String key);
+
+    void deleteData(String key);
+
+    Set<String> keys(String pattern);
+
+    Boolean lock(String key, Long timeout);
+
+    Boolean lock(String key, Object value, Long expireTime);
+
+    void unlock(String key);
 }
