7df4bb7b42710be5bda07328d13b98129d990bb9
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-10-01 23:22:34 +0900

    feat : 관심 내역 삭제 API 추가

--- backend/baro/baro/src/main/java/baro/baro/domain/wish_list/controller/WishListController.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/wish_list/controller/WishListController.java
@@ -3,12 +3,15 @@ package baro.baro.domain.wish_list.controller;
 import baro.baro.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static baro.baro.global.statuscode.SuccessCode.WISH_LIST_CREATED;
+import static baro.baro.global.statuscode.SuccessCode.WISH_LIST_DELETED;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +19,10 @@ public class WishListController {
     @PostMapping("/wish-list/{productId}")
     public ResponseEntity<?> wishListAdd(@PathVariable("productId") String productId) {
         return new ResponseEntity<>(ResponseDto.success(WISH_LIST_CREATED, null), CREATED);
+    }
+
+    @DeleteMapping("/wish-list/{productId}")
+    public ResponseEntity<?> wishListRemove(@PathVariable("productId") String productId) {
+        return new ResponseEntity<>(ResponseDto.success(WISH_LIST_DELETED, null), NO_CONTENT);
     }
 }
