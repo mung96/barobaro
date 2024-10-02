7fbdc999a1c8018cd445b9d6d3aa2f1ba5c1add7
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-10-02 22:36:21 +0900

    test : 대표 지역 변경 성공 테스트 작성

--- backend/baro/baro/src/test/java/baro/baro/domain/location/controller/LocationControllerTest.java
+++ backend/baro/baro/src/test/java/baro/baro/domain/location/controller/LocationControllerTest.java
@@ -1,6 +1,7 @@
 package baro.baro.domain.location.controller;
 
 import baro.baro.domain.location.dto.LocationReqDto;
+import baro.baro.domain.location.dto.request.DefaultLocationReq;
 import baro.baro.domain.location.dto.request.LocationsAddReq;
 import baro.baro.global.oauth.jwt.service.JwtService;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
@@ -28,15 +29,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static baro.baro.global.ResponseFieldUtils.getCommonResponseFields;
-import static baro.baro.global.statuscode.SuccessCode.LOCATION_SETTING_OK;
-import static baro.baro.global.statuscode.SuccessCode.SEARCH_LOCATION_OK;
+import static baro.baro.global.statuscode.SuccessCode.*;
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -189,6 +188,109 @@ public class LocationControllerTest {
                                 )
                                 .requestSchema(Schema.schema("지역 설정 Request"))
                                 .responseSchema(Schema.schema("지역 설정 Response"))
+                                .build()
+                        ))
+                );
+    }
+
+    @Test
+    public void 나의_지역_목록_조회_성공() throws Exception {
+        // given
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                get("/members/me/locations")
+                        .header("Authorization", jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.header.httpStatusCode").value(LOCATION_LIST_OK.getHttpStatusCode()))
+                .andExpect(jsonPath("$.header.message").value(LOCATION_LIST_OK.getMessage()))
+                .andDo(document(
+                        "나의 지역 목록 조회 성공",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("Location API")
+                                .summary("나의 지역 목록 조회 API")
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("JWT 토큰")
+                                )
+                                .responseFields(
+                                        getCommonResponseFields(
+                                                fieldWithPath("body.locations[].locationId").type(NUMBER)
+                                                        .description("지역 아이디"),
+                                                fieldWithPath("body.locations[].name").type(STRING)
+                                                        .description("시군동 이름"),
+                                                fieldWithPath("body.locations[].dong").type(STRING)
+                                                        .description("동 이름"),
+                                                fieldWithPath("body.locations[].isMain").type(BOOLEAN)
+                                                        .description("대표 지역 여부")
+                                        )
+                                )
+                                .requestSchema(Schema.schema("나의 지역 목록 조회 Request"))
+                                .responseSchema(Schema.schema("나의 지역 목록 조회 Response"))
+                                .build()
+                        ))
+                );
+    }
+
+    @Test
+    public void 대표_지역_변경_성공() throws Exception {
+        // given
+        DefaultLocationReq req = new DefaultLocationReq();
+        req.setLocationId(11010540L);
+
+        String content = objectMapper.writeValueAsString(req);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                post("/members/me/default-location")
+                        .header("Authorization", jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+        );
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.header.httpStatusCode").value(DEFAULT_LOCATION_UPDATE_OK.getHttpStatusCode()))
+                .andExpect(jsonPath("$.header.message").value(DEFAULT_LOCATION_UPDATE_OK.getMessage()))
+                .andDo(document(
+                        "대표 지역 변경 성공",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("Location API")
+                                .summary("대표 지역 변경 API")
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("JWT 토큰")
+                                )
+                                .requestFields(
+                                        fieldWithPath("locationId").description("변경할 지역 ID")
+                                )
+                                .responseFields(
+                                        getCommonResponseFields(
+                                                fieldWithPath("body.locations[].locationId").type(NUMBER)
+                                                        .description("지역 아이디"),
+                                                fieldWithPath("body.locations[].name").type(STRING)
+                                                        .description("시군동 이름"),
+                                                fieldWithPath("body.locations[].dong").type(STRING)
+                                                        .description("동 이름"),
+                                                fieldWithPath("body.locations[].isMain").type(BOOLEAN)
+                                                        .description("대표 지역 여부")
+                                        )
+                                )
+                                .requestSchema(Schema.schema("대표 지역 변경 Request"))
+                                .responseSchema(Schema.schema("대표 지역 변경 Response"))
                                 .build()
                         ))
                 );
