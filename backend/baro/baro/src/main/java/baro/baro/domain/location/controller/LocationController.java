3b260116e2ae60fcd78e2b8767c1beaecfc24309
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-10-02 21:55:57 +0900

    feat : 지역 설정 API 추가

--- backend/baro/baro/src/main/java/baro/baro/domain/location/controller/LocationController.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/location/controller/LocationController.java
@@ -1,18 +1,19 @@
 package baro.baro.domain.location.controller;
 
+import baro.baro.domain.location.dto.LocationDto;
 import baro.baro.domain.location.dto.SearchLocationDto;
+import baro.baro.domain.location.dto.request.LocationsAddReq;
+import baro.baro.domain.location.dto.response.LocationsAddRes;
 import baro.baro.domain.location.dto.response.SearchLocationRes;
 import baro.baro.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static baro.baro.global.statuscode.SuccessCode.LOCATION_SETTING_OK;
 import static baro.baro.global.statuscode.SuccessCode.SEARCH_LOCATION_OK;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -20,7 +21,6 @@ import static org.springframework.http.HttpStatus.OK;
 @RequiredArgsConstructor
 public class LocationController {
     @GetMapping("/search/locations")
-    @PostMapping("/members/signup")
     public ResponseEntity<?> searchLocation(@RequestParam("name") String name) {
         List<SearchLocationDto> searchLocationDtoList = new ArrayList<>();
 
@@ -36,5 +36,25 @@ public class LocationController {
         SearchLocationRes result = new SearchLocationRes(searchLocationDtoList);
 
         return new ResponseEntity<>(ResponseDto.success(SEARCH_LOCATION_OK, result), OK);
+    }
+
+    @PostMapping("/members/me/locations")
+    public ResponseEntity<?> LocationsAdd(@RequestBody LocationsAddReq locationsAddReq) {
+        List<LocationDto> locations = new ArrayList<>();
+
+        for(int i = 0; i < 3; i++) {
+            LocationDto location = LocationDto.builder()
+                    .locationId(11010530L + (10*i))
+                    .name("서울특별시 종로구 사직동(시군동)")
+                    .dong("사직동(동만)")
+                    .isMain(i == 0)
+                    .build();
+
+            locations.add(location);
+        }
+
+        LocationsAddRes result = new LocationsAddRes(locations);
+
+        return new ResponseEntity<>(ResponseDto.success(LOCATION_SETTING_OK, result), OK);
     }
 }
