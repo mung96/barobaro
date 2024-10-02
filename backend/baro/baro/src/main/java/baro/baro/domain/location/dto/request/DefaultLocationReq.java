78af5d4510adb946974e7e17bf4d9fbc9ef1a6f2
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-10-02 22:34:34 +0900

    feat : 대표 지역 변경 dto 추가

---
+++ backend/baro/baro/src/main/java/baro/baro/domain/location/dto/request/DefaultLocationReq.java
@@ -0,0 +1,10 @@
+package baro.baro.domain.location.dto.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class DefaultLocationReq {
+    private Long locationId;
+}
