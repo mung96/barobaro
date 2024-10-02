5de6563eecac0d3649415b8c8276ea80236c0c50
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-10-02 18:12:40 +0900

    feat : 관심 내역 조회 성공 코드 테스트 작성

--- backend/baro/baro/src/test/java/baro/baro/domain/wish_list/controller/WishListControllerTest.java
+++ backend/baro/baro/src/test/java/baro/baro/domain/wish_list/controller/WishListControllerTest.java
@@ -4,6 +4,7 @@ import baro.baro.global.oauth.jwt.service.JwtService;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.epages.restdocs.apispec.Schema;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,7 +27,9 @@ import java.util.List;
 import static baro.baro.global.ResponseFieldUtils.getCommonResponseFields;
 import static baro.baro.global.statuscode.SuccessCode.*;
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes.NUMBER;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
@@ -150,6 +153,63 @@ public class WishListControllerTest {
                                 )
                                 .requestSchema(Schema.schema("관심 내역 삭제 Request"))
                                 .responseSchema(Schema.schema("관심 내역 삭제 Response"))
+                                .build()
+                        ))
+                );
+    }
+
+    @Test
+    public void 관심내역_조회_성공() throws Exception {
+        // given
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                get("/wish-list")
+                        .header("Authorization", jwtToken)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.header.httpStatusCode").value(WISH_LIST_OK.getHttpStatusCode()))
+                .andExpect(jsonPath("$.header.message").value(WISH_LIST_OK.getMessage()))
+                .andDo(document(
+                        "관심내역 조회",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("WishList API")
+                                .summary("관심내역 조회 API")
+                                .requestHeaders(
+                                        headerWithName("Authorization")
+                                                .description("JWT 토큰")
+                                )
+                                .responseFields(
+                                        getCommonResponseFields(
+                                                fieldWithPath("body.products[].productId").type(NUMBER)
+                                                        .description("대여 물품 아이디"),
+                                                fieldWithPath("body.products[].productMainImage").type(JsonFormatTypes.STRING)
+                                                        .description("대여 물품 대표 이미지"),
+                                                fieldWithPath("body.products[].title").type(JsonFormatTypes.STRING)
+                                                        .description("대여 물품 제목"),
+                                                fieldWithPath("body.products[].startDate").type(JsonFormatTypes.STRING)
+                                                        .description("대여 시작일"),
+                                                fieldWithPath("body.products[].endDate").type(JsonFormatTypes.STRING)
+                                                        .description("대여 마감일"),
+                                                fieldWithPath("body.products[].dong").type(JsonFormatTypes.STRING)
+                                                        .description("거래 희망 동 정보"),
+                                                fieldWithPath("body.products[].uploadDate").type(JsonFormatTypes.STRING)
+                                                        .description("게시글 작성일"),
+                                                fieldWithPath("body.products[].rentalFee").type(NUMBER)
+                                                        .description("대여비"),
+                                                fieldWithPath("body.products[].wishCount").type(NUMBER)
+                                                        .description("찜한 사람 수")
+                                        )
+                                )
+                                .requestSchema(Schema.schema("관심 내역 조회 Request"))
+                                .responseSchema(Schema.schema("관심 내역 조회 Response"))
                                 .build()
                         ))
                 );
