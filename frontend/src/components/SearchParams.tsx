b7d025c291d68fe558ee340dca653757bca47558
한지웅 <wldnd2977@gmail.com>
2024-10-02 16:26:33 +0900

    fix : eslint fix - 2 S11P21A401-534

--- frontend/src/components/SearchParams.tsx
+++ frontend/src/components/SearchParams.tsx
@@ -6,22 +6,22 @@ import CategorySearch from '@/components/(category_button)/searchbar/CategorySea
 import { useSearchParams } from 'next/navigation';
 import ItemList from '@/components/ItemList';
 
-function getSearchData(data: string) {
+function useGetSearchData(data: string) {
   const searchParams = useSearchParams();
   return searchParams.get(data);
 }
 
 export default function CategoryDetailContent() {
-  const categoryData = getSearchData('category') || '';
-  const searchData = getSearchData('product') || '';
+  const categoryData = useGetSearchData('category') || '';
+  const searchData = useGetSearchData('product') || '';
 
   return (
     <NavBarLayout current="home">
       <div className="flex flex-col min-h-screen">
         <div className="fixed top-0 left-0 right-0 z-10 bg-white">
           <Header pageName="홈" hasPrevBtn hasSearchBtn hasAlertBtn />
-          <CategorySearch now={categoryData} searchData={searchData} />"
-          {searchData}" 검색 결과
+          <CategorySearch now={categoryData} searchData={searchData} />
+          {searchData} 검색 결과
           <ItemList data="search" />
         </div>
       </div>
