362a82f0054256de61a107a65bc80836273557fd
정지연 <jung_j_yeon@naver.com>
2024-10-04 15:15:38 +0900

    feat : 생성자 추가 S11P21A401-544

--- backend/baro/baro/src/main/java/baro/baro/domain/member/dto/response/SignUpInfoRes.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/member/dto/response/SignUpInfoRes.java
@@ -4,10 +4,12 @@ import baro.baro.domain.member.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignUpInfoRes {
     private String providerType;
 
