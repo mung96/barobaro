7063db2eca8d6aa973dad63b8f035cb68464ca95
박동환 <srpark13@naver.com>
2024-10-04 23:12:24 +0900

    feat : Security에 BouncyCastle Provider 추가 S11P21A401-540

--- backend/baro/baro/src/main/java/baro/baro/BaroApplication.java
+++ backend/baro/baro/src/main/java/baro/baro/BaroApplication.java
@@ -1,12 +1,16 @@
 package baro.baro;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.security.Security;
 
 @SpringBootApplication
 public class BaroApplication {
 
 	public static void main(String[] args) {
+		Security.addProvider(new BouncyCastleProvider());
 		SpringApplication.run(BaroApplication.class, args);
 	}
 
--- backend/baro/baro/src/test/java/baro/baro/BaroApplicationTests.java
+++ backend/baro/baro/src/test/java/baro/baro/BaroApplicationTests.java
@@ -1,13 +1,17 @@
 package baro.baro;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.security.Security;
 
 @SpringBootTest
 class BaroApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Security.addProvider(new BouncyCastleProvider());
 	}
 
 }
