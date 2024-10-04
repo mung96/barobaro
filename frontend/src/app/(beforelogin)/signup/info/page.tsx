5c3d64fd63f50d4bee4dc7a58cf4fd5734d52c31
mung96 <mung5079@gmail.com>
2024-10-04 18:06:21 +0900

    fix: build에러 수정 S11P21A401-499

--- frontend/src/app/(beforelogin)/signup/info/page.tsx
+++ frontend/src/app/(beforelogin)/signup/info/page.tsx
@@ -1,20 +1,20 @@
 'use client';
 
 import Image from 'next/image';
-import { useSearchParams } from 'next/navigation';
-import dynamic from 'next/dynamic';
+// import { useSearchParams } from 'next/navigation';
+// import dynamic from 'next/dynamic';
 import Logo from '@/../public/assets/png/barobaro_logo.png';
 
-const InfoComponent = dynamic(
-  () => import('@/components/signup/InfoComponent'),
-  {
-    ssr: false,
-  },
-);
+// const InfoComponent = dynamic(
+//   () => import('@/components/signup/InfoComponent'),
+//   {
+//     ssr: false,
+//   },
+// );
 
 function SignUpRedirectPage() {
   // API요청 보내서 닉네임이랑 프로필 가져오기
-  const email = decodeURI(useSearchParams().get('key')!);
+  // const email = decodeURI(useSearchParams().get('key')!);
 
   //   const response = getSignUpInfo(email);
 
@@ -27,7 +27,7 @@ function SignUpRedirectPage() {
     <section className="w-full h-[100dvh] flex flex-col justify-center items-center animate-pulse ">
       <Image src={Logo} alt="baro" width={300} height={300} />
       <p className="text-3xl">잠시만 기다려주세요.</p>
-      <InfoComponent email={email} />
+      {/* <InfoComponent email={email} /> */}
     </section>
   );
 }
