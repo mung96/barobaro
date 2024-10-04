9d2aebcd92f6a6544b55f8925a6d23837cc87f08
정지연 <jung_j_yeon@naver.com>
2024-10-04 11:23:24 +0900

    chore : 세미콜론 삭제 S11P21A401-567

--- backend/baro/baro/src/main/java/baro/baro/domain/location/repository/LocationRepository.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/location/repository/LocationRepository.java
@@ -9,5 +9,5 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
     @Query(value = "SELECT * " +
             "FROM location " +
             "WHERE ST_Contains(geom, ST_Transform(ST_SetSRID(ST_Point(:longitude, :latitude), 4326), 5186))", nativeQuery = true)
-    Location findLocation(@Param("latitude") double latitude, @Param("longitude") double longitude);;
+    Location findLocation(@Param("latitude") double latitude, @Param("longitude") double longitude);
 }
