0aad28466cc60af6e25934d7304cfebfef448483
한지웅 <wldnd2977@gmail.com>
2024-10-04 13:08:18 +0900

    refactor : post detail 타입, 변수 S11P21A401-557

--- frontend/src/app/(afterlogin)/post/[post_id]/page.tsx
+++ frontend/src/app/(afterlogin)/post/[post_id]/page.tsx
@@ -6,7 +6,6 @@ import Profile from '@/components/user/Profile';
 import PostContent from '@/components/post/PostContent';
 import ContractCondition from '@/components/post/ContractCondition';
 import { useState } from 'react';
-import { ModalType } from '@/types/overlay/modal';
 import PostCheckModal from '@/components/modal/PostCheckModal';
 import Header from '@/components/Header';
 import LikeButton from '@/components/(SVG_component)/LikeButton';
@@ -14,10 +13,37 @@ import CalendarSVG from '@/components/(SVG_component)/Calendar';
 
 export default function PostDetail() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalType, setModalType] = useState<ModalType>('needPassword');
+  const modalType = 'needPassword';
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
-
+  // 해당 내용은 API  호출후 활용 가정
+  const layoutData = {
+    startDate: '24.10.21',
+    endDate: '24.10.22',
+    rentalFee: 10000,
+  };
+  const contentData = {
+    title: '캐럿봉 대여합니당',
+    productStatus: 'FINISH',
+    wishCount: 3,
+    content: `캐럿봉 대여합니다 \n \n 세븐틴 콘서트장에서도 직거래 가능합니다. \n 대여기간 보시고 연락주세요~~!`,
+    place: '고척스카이돔 중앙출입문 C게이트 앞',
+    latitude: 37.5,
+    longitude: 126.87,
+  };
+  const contractData = {
+    isWriteContract: true,
+    contractCondition: {
+      repairVendor: '제조사 또는 공식 수입사의 AS 센터',
+      overdueCriteria: 5,
+      overdueFee: 2,
+      theftCriteria: 7,
+      refundDeadline: 7,
+    },
+  };
+  // 작성자와 유저 아이디 비교하고, 수정 삭제버튼 표시 OX
+  const userId: number = 1;
+  const writerId: number = 2;
   return (
     <>
       <Header pageName="게시글 목록" hasPrevBtn hasSearchBtn hasAlertBtn />
@@ -32,27 +58,34 @@ export default function PostDetail() {
         <div className="flex w-full">
           <Profile hasEmail={false} hasEditBtn={false} />
           <div className="flex-1" />
-          <div className="flex">
-            <button
-              type="button"
-              className="text-[10px] underline text-gray-300 me-1 w-[30px]"
-              onClick={() => openModal()}
-            >
-              수정
-            </button>
-            <button
-              type="button"
-              className="text-[10px] underline text-gray-300 w-[30px]"
-            >
-              삭제
-            </button>
-          </div>
+          {writerId === userId ? (
+            <div className="flex">
+              <button
+                type="button"
+                className="text-[10px] underline text-gray-300 me-1 w-[30px]"
+                onClick={() => openModal()}
+              >
+                수정
+              </button>
+              <button
+                type="button"
+                className="text-[10px] underline text-gray-300 w-[30px]"
+              >
+                삭제
+              </button>
+            </div>
+          ) : null}
         </div>
         <PictureCarousel />
         <div className="bg-gray-500 w-[90%] h-[1px] my-3" />
-        <PostContent />
-        <KakaoMap width="85%" height="20dvh" lat={37.498333} lng={126.866667} />
-        <ContractCondition />
+        <PostContent data={contentData} />
+        <KakaoMap
+          width="85%"
+          height="20dvh"
+          lat={contentData.latitude}
+          lng={contentData.longitude}
+        />
+        <ContractCondition data={contractData} />
       </div>
       <div className="-z-0 fixed bottom-0 -z-0 max-w-[500px] w-[100%] h-[60px] bg-white flex items-center">
         <div className="flex items-center justify-center w-full">
@@ -63,9 +96,13 @@ export default function PostDetail() {
           <div className="flex flex-col flex-1 mx-3">
             <div className="flex items-center">
               <CalendarSVG />
-              <p className="text-gray-300 text-[12px]">24.10.21 ~ 24.10.23</p>
+              <p className="text-gray-300 text-[12px]">
+                {layoutData.startDate}~{layoutData.endDate}
+              </p>
             </div>
-            <p className="text-black-100 text-[16px] font-bold">20,000원/일</p>
+            <p className="text-black-100 text-[16px] font-bold">
+              {layoutData.rentalFee}원/일
+            </p>
           </div>
           <button
             type="button"
