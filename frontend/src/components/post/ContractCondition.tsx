a5451e72fdd4ae0b9f842dda58131e8b3f41aa87
한지웅 <wldnd2977@gmail.com>
2024-10-04 13:08:18 +0900

    refactor : post detail 타입, 변수 S11P21A401-557

--- frontend/src/components/post/ContractCondition.tsx
+++ frontend/src/components/post/ContractCondition.tsx
@@ -1,18 +1,6 @@
-export default function ContractCondition() {
-  const data = {
-    contract: true,
-    method: ['직거래', '택배'],
-    condition: {
-      damaged: {
-        place: '제조사 또는 공식 수입사의 AS 센터',
-        charge: '청구 시점 기준 5일 이내 지급',
-      },
-      delayed: {
-        criteria: '대여 기간 종료 후 4일',
-        price: '대여 제품 1일 가격의 3배',
-      },
-    },
-  };
+import { ContractContent } from '@/types/apis/productResponse';
+
+export default function ContractCondition({ data }: { data: ContractContent }) {
   return (
     <section className="w-[85%] my-[12px] text-black-100">
       <h1 className="font-bold text-[12px]">계약조건</h1>
@@ -21,12 +9,12 @@ export default function ContractCondition() {
           <div>
             <h2 className="text-[11px]">전자계약서 작성 여부</h2>
             <p className="text-[10px] text-gray-300 ms-2 my-1">
-              전자계약서 작성 {data.contract ? 'O' : 'X'}
+              전자계약서 작성 {data.isWriteContract ? 'O' : 'X'}
             </p>
           </div>
           <div>
             <h2 className="text-[11px]">반납 희망 방법</h2>
-            <p className="text-[10px] text-gray-300 ms-2 my-1">{data.method}</p>
+            <p className="text-[10px] text-gray-300 ms-2 my-1">직거래, 택배</p>
           </div>
           <div>
             <h2 className="text-[11px]">계약 조건</h2>
@@ -37,11 +25,16 @@ export default function ContractCondition() {
               <div className="flex mx-5">
                 <div className="flex-1">
                   <h3 className="text-gray-300 text-[10px]">수리업체</h3>
-                  <p className="text-[8px]">{data.condition.damaged.place}</p>
+                  <p className="text-[8px]">
+                    {data.contractCondition.repairVendor}
+                  </p>
                 </div>
                 <div className="flex-1">
                   <h3 className="text-gray-300 text-[10px]">청구 비용</h3>
-                  <p className="text-[8px]">{data.condition.damaged.charge}</p>
+                  <p className="text-[8px]">
+                    청구 시점 기준 {data.contractCondition.overdueCriteria}일
+                    이내 지급
+                  </p>
                 </div>
               </div>
               <h2 className="text-gray-300 text-[10px] bg-gray-500 rounded-[30px] w-[50px] h-[16px] flex justify-center items-center ms-2 my-1">
@@ -51,12 +44,14 @@ export default function ContractCondition() {
                 <div className="flex-1">
                   <h3 className="text-gray-300 text-[10px]">기준</h3>
                   <p className="text-[8px]">
-                    {data.condition.delayed.criteria}
+                    대여 기간 종료 후 {data.contractCondition.theftCriteria}일
                   </p>
                 </div>
                 <div className="flex-1">
                   <h3 className="text-gray-300 text-[10px]">청구 비용</h3>
-                  <p className="text-[8px]">{data.condition.delayed.price}</p>
+                  <p className="text-[8px]">
+                    1일 이용 가격의 {data.contractCondition.overdueFee}배
+                  </p>
                 </div>
               </div>
             </div>
