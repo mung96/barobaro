2ee95092a59375b940e82991f07d08710afd6c61
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-10-06 00:11:00 +0900

    feat : 나의 지역 조회 서비스 함수 작성

--- backend/baro/baro/src/main/java/baro/baro/domain/location/service/LocationService.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/location/service/LocationService.java
@@ -2,7 +2,9 @@ package baro.baro.domain.location.service;
 
 import baro.baro.domain.location.dto.request.LocationsAddReq;
 import baro.baro.domain.location.dto.response.LocationsAddRes;
+import baro.baro.domain.location.dto.response.MyLocationListRes;
 
 public interface LocationService {
     LocationsAddRes addLocations(LocationsAddReq locationsAddReq, Long memberId);
+    MyLocationListRes findLocations(Long memberId);
 }
