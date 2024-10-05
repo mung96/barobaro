2d4c3d2cd4d498b4cae54ea2e9dbfb82609d479b
박동환 <srpark13@naver.com>
2024-10-05 22:04:33 +0900

    fix : BouncyCastle 프로바이더 등록 부분 에러 수정 S11P21A401-540

--- backend/baro/baro/src/main/java/baro/baro/BaroApplication.java
+++ backend/baro/baro/src/main/java/baro/baro/BaroApplication.java
@@ -1,16 +1,12 @@
 package baro.baro;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.security.Security;
 
 @SpringBootApplication
 public class BaroApplication {
 
 	public static void main(String[] args) {
-		Security.addProvider(new BouncyCastleProvider());
 		SpringApplication.run(BaroApplication.class, args);
 	}
 
