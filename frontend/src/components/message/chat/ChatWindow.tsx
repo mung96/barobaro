3caf184ce162cde0ffd3a8773f9b65b4fd5dc755
이주영 <git.leej.young@gmail.com>
2024-10-06 19:18:24 +0900

    feat: 채팅방 사진 추가 기능 S11P21A401-491

--- frontend/src/components/message/chat/ChatWindow.tsx
+++ frontend/src/components/message/chat/ChatWindow.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import AttatchImage from '@/components/(SVG_component)/(message)/(chat)/AttatchImage';
 import SendButton from '@/components/(SVG_component)/(message)/(chat)/SendButton';
 import webSocketClient from '@/utils/webSocketClient';
 import useChatWindowModel from '@/hooks/message/chat/useChatWindowModel';
+import useChatFileModel from '@/hooks/message/chat/useChatFileModel';
+import ChatImageList from './ChatImageList';
+import ChatImageAdd from './ChatImageAdd';
 
 type ChatWindowParam = {
   client: webSocketClient | null;
@@ -12,27 +14,56 @@ export default function ChatWindow({ client }: ChatWindowParam) {
   const { chatValue, messageRef, handleChatValue, sendChat, handleEnterPress } =
     useChatWindowModel(client);
 
+  const {
+    files,
+    changeFile,
+    //  handleDragEnd, // 채팅 사진 리스트 내에서는 드래그 기능 사용 X (너무 작음)
+    deleteFileByIndex,
+    activeTextArea, // text 채팅을 사용하는지 여부 (이미지가 있으면 false)
+    chatWarning, // 이미지 관련 warning 표시
+    warningHandler,
+  } = useChatFileModel();
+
   return (
     <div className="box-border ">
       <div className=" bg-white p-2">
         <div className="w-full h-[6vh] flex justify-center">
           <div className="w-[95%] h-full flex items-center justify-around rounded-2xl border border-gray-500 bg-gray-400 box-border">
-            <AttatchImage className="w-2/12" />
-            <input
-              type="text"
-              className="w-8/12 bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-30"
-              onChange={handleChatValue}
-              value={chatValue}
-              ref={messageRef}
-              onKeyDown={handleEnterPress}
-            />
+            <div className="flex items-center justify-center w-2/12">
+              <ChatImageAdd width="56px" height="56px" addFile={changeFile} />
+            </div>
+            {activeTextArea ? (
+              <input
+                type="text"
+                className="w-8/12 bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-30"
+                onChange={handleChatValue}
+                value={chatValue}
+                ref={messageRef}
+                onKeyDown={handleEnterPress}
+                placeholder={chatWarning || ''}
+                onFocus={() => warningHandler('')}
+              />
+            ) : (
+              <div className="w-8/12">
+                <ChatImageList
+                  width="40px"
+                  height="40px"
+                  images={files}
+                  deleteFile={deleteFileByIndex}
+                  //      dropEnd={handleDragEnd}
+                />
+              </div>
+            )}
             <button
               type="button"
               onClick={sendChat}
               className="w-2/12 focus:outline-none"
               disabled={!chatValue}
             >
-              <SendButton active={!!chatValue} className="w-full" />
+              <SendButton
+                active={!!chatValue || !activeTextArea}
+                className="w-full"
+              />
             </button>
           </div>
         </div>
