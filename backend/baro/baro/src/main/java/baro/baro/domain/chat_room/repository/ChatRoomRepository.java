86ce5e1c91f3dfe3e267ae89425ccc29b4134d51
이윤지 <yunji383614@gmail.com>
2024-10-06 17:16:29 +0900

    feat : 빌려준 내역 리스트 조회 API 생성

--- backend/baro/baro/src/main/java/baro/baro/domain/chat_room/repository/ChatRoomRepository.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/chat_room/repository/ChatRoomRepository.java
@@ -5,11 +5,14 @@ import baro.baro.domain.chat_room.entity.RentalStatus;
 import baro.baro.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatRoomRepositoryCustom {
     Optional<ChatRoom> findByProductIdAndOwnerIdAndRentalId(Long productId, Long ownerId, Long rentalId);
 
-    List<ChatRoom> findByRentalIdAndRentalStatusNot(Long memberId, RentalStatus rentalStatus);
+    List<ChatRoom> findByRentalIdAndRentalStatusNot(Long rentalId, RentalStatus rentalStatus);
+
+    List<ChatRoom> findByOwnerIdAndRentalStatusNot(Long ownerId, RentalStatus rentalStatus);
 }
