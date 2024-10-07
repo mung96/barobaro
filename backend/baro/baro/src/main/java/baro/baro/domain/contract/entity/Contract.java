6c384cd78444ffde3eea2ec90e5c4bac1b16d5a3
박동환 <srpark13@naver.com>
2024-10-07 15:29:18 +0900

    feat : contract 엔티티 필드 수정 S11P21A401-524

--- backend/baro/baro/src/main/java/baro/baro/domain/contract/entity/Contract.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/contract/entity/Contract.java
@@ -7,7 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -19,31 +19,13 @@ public class Contract {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "contract_id")
-    private int id;
+    private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
 
-    private String productName;
-
-    private String serialNumber;
-
-    private LocalDate startDate;
-
-    private LocalDate endDate;
-
-    private Integer rentalFee;
-
-    private String repairVendor;
-
-    private Integer overdueCriteria;
-
-    private Integer overdueFee;
-
-    private Integer theftCriteria;
-
-    private Integer refundDeadline;
+    private LocalDateTime createdAt;
 
     private String contractKeyName;
 }
