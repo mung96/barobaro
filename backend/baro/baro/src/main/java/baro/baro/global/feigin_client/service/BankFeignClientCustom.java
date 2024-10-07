aa09dd793926633cc3700935bccab37acfc9ff24
이윤지 <yunji383614@gmail.com>
2024-10-07 16:36:22 +0900

    feat : 금융망 수시입출금 상품 등록 S11P21A401-589

--- backend/baro/baro/src/main/java/baro/baro/global/feigin_client/service/BankFeignClientCustom.java
+++ backend/baro/baro/src/main/java/baro/baro/global/feigin_client/service/BankFeignClientCustom.java
@@ -2,8 +2,10 @@ package baro.baro.global.feigin_client.service;
 
 import baro.baro.global.feigin_client.dto.request.BankMemberAddReq;
 import baro.baro.global.feigin_client.dto.request.BankMemberDetailsReq;
+import baro.baro.global.feigin_client.dto.request.BankProductAddReq;
 import baro.baro.global.feigin_client.dto.response.BankMemberAddRes;
 import baro.baro.global.feigin_client.dto.response.BankMemberDetailsRes;
+import baro.baro.global.feigin_client.dto.response.BankProductAddRes;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,4 +16,7 @@ public interface BankFeignClientCustom {
 
     @PostMapping("/member/search")
     BankMemberDetailsRes findBankMember(BankMemberDetailsReq req); // userId로 userKey 찾기 용도
+
+    @PostMapping("/edu/demandDeposit/createDemandDeposit")
+    BankProductAddRes addBankProduct(BankProductAddReq req);
 }
