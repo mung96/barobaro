7a9bfcf0fea3c6372e5aa0c0988db079aaafc761
mung96 <mung5079@gmail.com>
2024-10-07 22:13:49 +0900

    feat: localstorage에 토큰 담는 기능 구현 S11P21A401-598

--- frontend/src/components/signup/RedirectComponent.tsx
+++ frontend/src/components/signup/RedirectComponent.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import Image from "next/image";
-
-
 import Logo from '@/../public/assets/png/barobaro_logo.png';
 import { useSearchParams } from "next/navigation";
 
@@ -10,9 +8,9 @@ const RedirectComponent = () => {
 const searchParams = useSearchParams();
 localStorage.setItem('token',searchParams.get('token')!);
 
-return <section className="w-full h-[100dvh] flex flex-col justify-center items-center">
-    <Image src={Logo} alt="baro" width={200} height={200} className="animate-pulse" />
-    <p>잠시만 기다려주세요.</p>
+return <section className="w-full h-[100dvh] flex flex-col justify-center items-center  animate-pulse">
+    <Image src={Logo} alt="baro" width={280} height={280} />
+    <p className="text-2xl">잠시만 기다려주세요.</p>
   </section> 
 }
 
