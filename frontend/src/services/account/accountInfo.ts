16de9c8a28f2cdeb9e23e36544235b438974b8fc
한지웅 <wldnd2977@gmail.com>
2024-10-07 19:47:14 +0900

    design: KakaoBankmark S11P21A401-579

--- frontend/src/services/account/accountInfo.ts
+++ frontend/src/services/account/accountInfo.ts
@@ -1,6 +1,7 @@
 import KB from '@/../public/assets/png/bank/KB_mark.png';
 import Shinhan from '@/../public/assets/png/bank/Shinhan_mark.png';
 import SSAFY from '@/../public/assets/png/bank/SSAFY_mark.png'
+import KakaoBank from '@/../public/assets/png/bank/Kakao_mark.png'
 import { StaticImageData } from 'next/image';
 
 const bankColor: { [key: string]: string } = {
@@ -10,12 +11,11 @@ const bankColor: { [key: string]: string } = {
   카카오뱅크: '#ffbc00',
 };
 
-// TODO : 카카오뱅크 이미지 가져와서 구현할 것
 const bankImage: { [key: string]: StaticImageData } = {
   국민은행: KB,
   신한은행: Shinhan,
   싸피은행: SSAFY,
-  카카오뱅크: SSAFY,
+  카카오뱅크: KakaoBank,
 };
 
 export function convertAccountColor(bank: string) {
