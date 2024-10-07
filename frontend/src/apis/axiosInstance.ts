58bce1f7af0e8361148cbad507f1e36ecbaaab9f
mung96 <mung5079@gmail.com>
2024-10-08 08:15:44 +0900

    feat: 비로그인 instance 생성 S11P21A401-598

--- frontend/src/apis/axiosInstance.ts
+++ frontend/src/apis/axiosInstance.ts
@@ -3,12 +3,19 @@ import {
   KAKAO_BASE_URL,
   KAKAO_REST_API_KEY,
   NETWORK,
+  SERVER_BASE_URL,
   // SERVER_BASE_URL,
 } from '@/constants/api';
 
 export const kakaoInstance = axios.create({
   baseURL: KAKAO_BASE_URL,
   headers: { Authorization: `KakaoAK ${KAKAO_REST_API_KEY}` },
+});
+
+export const nonLoginInstance = axios.create({
+  baseURL: SERVER_BASE_URL, // 기본 URL
+  withCredentials: true, // 도메인이 다른 서버에 쿠키 요청시 필요
+  timeout: NETWORK.TIMEOUT, // TimeOut 시간이 지나도 응답이 안오면 에러처리됨
 });
 
 export const axiosInstance = axios.create({
@@ -18,8 +25,8 @@ export const axiosInstance = axios.create({
   timeout: NETWORK.TIMEOUT, // TimeOut 시간이 지나도 응답이 안오면 에러처리됨
   headers: {
     Authorization:
-        // `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}`
-        `Bearer ${localStorage.getItem('token')}`,
+        `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}`
+        // `Bearer ${localStorage.getItem('token')}`,
   },
 });
 
