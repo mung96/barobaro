f2aaf41b3d13be293ce553f1deaf013ba6d0c080
박동환 <srpark13@naver.com>
2024-10-07 15:28:36 +0900

    feat : pdf 생성 요청 DTO를 만드는 static 메소드 추가 S11P21A401-524

--- backend/baro/baro/src/main/java/baro/baro/global/dto/PdfCreateDto.java
+++ backend/baro/baro/src/main/java/baro/baro/global/dto/PdfCreateDto.java
@@ -1,10 +1,15 @@
 package baro.baro.global.dto;
 
+import baro.baro.domain.contract.dto.ContractApplicationDto;
+import baro.baro.domain.contract.entity.ContractCondition;
+import baro.baro.domain.member.entity.Member;
+import baro.baro.domain.product.entity.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Data
 @Builder
@@ -26,5 +31,29 @@ public class PdfCreateDto {
     private Integer overdueFee;
     private Integer theftCriteria;
     private Integer refundDeadline;
+
+    public static PdfCreateDto toDto(Long chatRoomId, Member owner, Member rental, Product product, ContractApplicationDto contractApplicationDto, ContractCondition contractCondition) {
+        return PdfCreateDto.builder()
+                .chatRoomId(chatRoomId)
+                .ownerName(owner.getName())
+                .ownerTel(owner.getPhoneNumber())
+                .ownerEmail(owner.getEmail())
+                .rentalName(rental.getName())
+                .rentalTel(rental.getPhoneNumber())
+                .rentalEmail(rental.getEmail())
+                .productName(contractCondition.getProductName())
+                .productSerialNumber(contractCondition.getSerialNumber())
+                .totalRentalPrice(
+                        ChronoUnit.DAYS.between(contractApplicationDto.getDesiredStartDate(), contractApplicationDto.getDesiredEndDate())
+                                * product.getRentalFee()
+                )
+                .rentalStartDate(contractApplicationDto.getDesiredStartDate())
+                .rentalEndDate(contractApplicationDto.getDesiredEndDate())
+                .overdueCriteria(contractCondition.getOverdueCriteria())
+                .overdueFee(contractCondition.getOverdueFee())
+                .theftCriteria(contractCondition.getTheftCriteria())
+                .refundDeadline(contractCondition.getRefundDeadline())
+                .build();
+    }
 }
 
