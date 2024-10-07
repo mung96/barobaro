9c617016131ef45c2076f772d1d5a6a0b41c722b
mung96 <mung5079@gmail.com>
2024-10-07 09:05:29 +0900

    feat: locationAPI 타입 지정 S11P21A401-577

--- frontend/src/apis/locationApi.ts
+++ frontend/src/apis/locationApi.ts
@@ -1,16 +1,17 @@
 import { axiosInstance } from "@/apis/axiosInstance"
 import { END_POINT } from "@/constants/api"
+import { DefaultLocationResponse, DefaultLocationRequest, MyLocationRequest, SearchLocationRequest, MyLocationsResponse, SearchResultDongResponse } from "@/types/apis/location"
 
-export const postDefaultLocation = async () => {
-    return await axiosInstance.post(END_POINT.DEFAULT_LOCATION,{})
+export const postDefaultLocation = async (data:DefaultLocationRequest) => {
+    return await axiosInstance.post<DefaultLocationResponse>(END_POINT.DEFAULT_LOCATION,{data})
 }
 export const getMyLocations = async () => {
-    return await axiosInstance.get(END_POINT.MY_LOCATIONS,{})
+    return await axiosInstance.get<MyLocationsResponse>(END_POINT.MY_LOCATIONS)
 }
 
-export const postMyLocation = async () => {
-    return await axiosInstance.post(END_POINT.MY_LOCATIONS,{})
+export const postMyLocation = async (data:MyLocationRequest) => {
+    return await axiosInstance.post<MyLocationsResponse>(END_POINT.MY_LOCATIONS,{data})
 }
-export const getDongList = async () => {
-    return await axiosInstance.post(END_POINT.SEARCH_LOCATION,{})
+export const getDongList = async (data:SearchLocationRequest) => {
+    return await axiosInstance.post<SearchResultDongResponse>(END_POINT.SEARCH_LOCATION,{params:{data}})
 }
