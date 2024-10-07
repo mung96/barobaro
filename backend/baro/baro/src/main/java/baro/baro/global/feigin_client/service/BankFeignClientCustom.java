b92e1af4d9157f7aec2d07faef9a7e9df8262861
이윤지 <yunji383614@gmail.com>
2024-10-07 14:09:57 +0900

    feat : SSAFY 금융망에 테스트용 사용자 등록 S11P21A401-587

--- backend/baro/baro/src/main/java/baro/baro/global/feigin_client/service/BankFeignClientCustom.java
+++ backend/baro/baro/src/main/java/baro/baro/global/feigin_client/service/BankFeignClientCustom.java
@@ -1,7 +1,12 @@
 package baro.baro.global.feigin_client.service;
 
+import baro.baro.global.feigin_client.dto.request.BankMemberAddReq;
+import baro.baro.global.feigin_client.dto.response.BankMemberAddRes;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "bankFeignClientCustom", url = "https://finopenapi.ssafy.io/ssafy/api/v1")
 public interface BankFeignClientCustom {
+    @PostMapping("/member")
+    BankMemberAddRes addBankMember(BankMemberAddReq req);
 }
--- backend/baro/baro/src/main/java/baro/baro/global/feigin_client/service/FeignClientService.java
+++ backend/baro/baro/src/main/java/baro/baro/global/feigin_client/service/FeignClientService.java
@@ -1,12 +1,18 @@
 package baro.baro.global.feigin_client.service;
 
+import baro.baro.global.feigin_client.dto.request.BankMemberAddReq;
+import baro.baro.global.feigin_client.dto.response.BankMemberAddRes;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class FeignClientService {
     private final BankFeignClientCustom bankFeignClientCustom;
+
+    @Value("${BANK_API_KEY}")
+    private String apiKey;
 
     public void callBankApi() {
         // 초기 데이터 세팅 예정
