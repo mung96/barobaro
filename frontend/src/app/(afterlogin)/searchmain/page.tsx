115563b205e4ef4881bfcedc6e90eb2a507c7890
한지웅 <wldnd2977@gmail.com>
2024-10-08 16:55:28 +0900

    feat : search main S11P21A401-601

---
+++ frontend/src/app/(afterlogin)/searchmain/page.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import Header from "@/components/Header";
+import useSearchModel from "@/hooks/products/useSearchModel";
+import {useState, useEffect} from "react";
+import {useRouter} from 'next/navigation'
+import Search from "@/components/(SVG_component)/Search";
+
+export default function SearchMain(){
+    const { handleSearch, goSearchWord, search, goSearch, recommendData } = useSearchModel();
+    const [recommendKeyword, setRecommendKeyword] = useState<any>([{name : ''}]);
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        goSearch();
+    }
+
+    useEffect(() => {
+        console.log('searchMain', recommendData)
+        console.log(typeof recommendData)
+        if (recommendData === '') {
+            setRecommendKeyword([{name : ''}])
+        } else {
+            setRecommendKeyword(recommendData)
+        }
+    }, [recommendData]);
+    // TODO : 추천 검색어를 표시하는 타이밍이 다소 늦음
+    return (
+        <>
+            <Header pageName="홈" hasPrevBtn hasSearchBtn hasAlertBtn/>
+            <section className="flex flex-col items-center w-full mb-20">
+                <div className="w-[90%] h-[35px] rounded-[5px] bg-gray-400 flex justify-start items-center">
+                    <div className="mx-1">
+                        <Search/>
+                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            name="search_input"
+                            type="search"
+                            id="search_input"
+                            placeholder="검색어를 입력해주세요."
+                            onChange={(e) => handleSearch(e.target.value)}
+                            className="w-full outline-none [&::-webkit-search-cancel-button]:appearance-none bg-transparent"
+                            required
+                        />
+                    </form>
+                </div>
+                {recommendKeyword.length > 0 && recommendKeyword[0].name !== '' && (
+                    <div className="mt-4 flex flex-col items-start w-[90%]">
+                        <ul>
+                            {recommendKeyword.map((keyword : any, index : any) => (
+                                <div key={index} onClick={() => goSearchWord("all", keyword.name)} className="flex m-5">
+                                    <div className="mx-1">
+                                        <Search/>
+                                    </div>
+                                    <div>
+                                        {keyword.name}
+                                    </div>
+
+                                </div>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+            </section>
+        </>
+
+    )
+}
--- frontend/src/hooks/products/useSearchModel.ts
+++ frontend/src/hooks/products/useSearchModel.ts
@@ -6,10 +6,16 @@ import {getSuggest} from "@/apis/searchSuggestApi";
 function useSearchModel(): SearchHookType {
   const router = useRouter();
   const [search, setSearch] = useState('');
-  const handleSearch = (value: string) => {
+  const [recommendData, setRecommendData] = useState<any>('');
+  const handleSearch = async (value: string) => {
     setSearch(value);
-    console.log(value)
-    getSuggest(value)
+    const res = await getSuggest(value)
+    if (search.length === 0) {
+      setRecommendData("")
+    } else {
+      setRecommendData(res)
+    }
+    console.log('useSearchModel', res)
   };
   const goSearch = () => {
     if (search.length !== 0) {
@@ -20,10 +26,20 @@ function useSearchModel(): SearchHookType {
       router.push(`/search?${queryParams.toString()}`);
     }
   };
+  const goSearchWord = (category: any, word: any) => {
+    const queryParams = new URLSearchParams({
+      category: category,
+      product: word,
+    });
+    router.push(`/search?${queryParams.toString()}`);
+  }
   return {
     search,
     handleSearch,
     goSearch,
+    setRecommendData,
+    recommendData,
+    goSearchWord
   };
 }
 
