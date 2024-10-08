a48b5fb5535334d4cd3331c48e1b8308a5752716
mung96 <mung5079@gmail.com>
2024-10-08 16:11:54 +0900

    refactor: 필요없는 코드 삭제 S11P21A401-582

--- frontend/src/components/post/PostInfoInput.tsx
+++ frontend/src/components/post/PostInfoInput.tsx
@@ -1,11 +1,10 @@
-import {  useForm } from 'react-hook-form';
 import CategoryTagList from '@/components/post/CategoryTagList';
 import PostBody from '@/components/post/PostBody';
 import PostTitleInput from '@/components/post/PostTitleInput';
 import ProductImageList from '@/components/post/ProductImageList';
 import Button from '@/components/shared/Button';
 import useFileModel from '@/hooks/shared/useFileModel';
-import { PostInfo, ProductCategory } from '@/types/domains/product';
+import {  ProductCategory } from '@/types/domains/product';
 import { PostFormFields, StepProps } from '@/hooks/post/usePostFormModel';
 
 type Props = {
@@ -15,50 +14,6 @@ type Props = {
 function PostInfoInput({ onNext,fields,errors }: Props) {
   const { files, changeFile, handleDragEnd, deleteFileByIndex } =
     useFileModel();
-
-  // const { getValues, control, formState: { errors, isValid }} = useForm<PostInfo>({
-  //   mode:'onChange'
-  // });
-
-  // const { field: title, fieldState: titleState } = useController<PostInfo>({
-  //   control,
-  //   name: 'title',
-  //   defaultValue:'',
-  //   rules: {
-  //     required: '게시글 제목을 입력해주세요.',
-  //     minLength: { value: 2, message: '제목은 2자 이상 입력해주세요' },
-  //     maxLength: { value: 20, message: '제목은 20자 이하 입력해주세요' },
-  //   },
-  // });
-
-
-  // const { field: images, fieldState: imagesState } = useController<PostInfo>({
-  //   control,
-  //   name: 'images',
-  // });
-
-  // const { field: category, fieldState: categoryState } = useController<PostInfo>({
-  //   control,
-  //   name: 'category',
-  //   defaultValue:'TELESCOPE',
-  //   rules: {
-  //     required: '카테고리를 선택해주세요',
-  //   },
-  // });
-
-
-  // const { field: body, fieldState: bodyState } = useController<PostInfo>({
-  //   control,
-  //   name: 'body',
-  //   defaultValue:'',
-  //   rules: {
-  //     required: '게시글 내용을 입력해주세요.',
-  //     minLength: { value: 1, message: '게시글은 1자 이상 입력해주세요' },
-  //     maxLength: { value: 1000, message: '게시글은 1000자 이하 입력해주세요' },
-  //   },
-  // }); 
- 
-
   return (
     <div className="flex flex-col gap-8">
       <PostTitleInput
