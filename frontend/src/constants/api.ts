7662370360ff90c0e005dc5d348ec5118dcff64f
mung96 <mung5079@gmail.com>
2024-10-08 13:08:38 +0900

    feat: port 번호에 따라 토큰 요청 다르게 보내는 로직 S11P21A401-605

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -2,6 +2,7 @@ export const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
 export const SERVER_BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 export const NEXT_BASE_URL = process.env.NEXT_PUBLIC_FRONT_URL;
 export const KAKAO_BASE_URL = 'https://dapi.kakao.com/v2/';
+export const DummyToken = process.env.NEXT_PUBLIC_AUTH_TOKEN;
 
 export const END_POINT = {
   LOCATION: 'local/search/keyword.json',
