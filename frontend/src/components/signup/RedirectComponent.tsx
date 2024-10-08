8210bf16f3d5ecf1d2d236ef1d5713057bf3b989
mung96 <mung5079@gmail.com>
2024-10-08 13:08:38 +0900

    feat: port 번호에 따라 토큰 요청 다르게 보내는 로직 S11P21A401-605

--- frontend/src/components/signup/RedirectComponent.tsx
+++ frontend/src/components/signup/RedirectComponent.tsx
@@ -3,13 +3,16 @@
 import Image from "next/image";
 import Logo from '@/../public/assets/png/barobaro_logo.png';
 import { useRouter, useSearchParams } from "next/navigation";
-
+import { jwtDecode } from "jwt-decode";
 
 const RedirectComponent = () => {
   const searchParams = useSearchParams();
   localStorage.setItem('token',searchParams.get('token')!);
-  const router = useRouter();
-  router.push('/home');
+  const decoded = jwtDecode(searchParams.get('token')!);
+  console.log(decoded);
+
+  // const router = useRouter();
+  // router.push('/home');
 
 return <section className="w-full h-[100dvh] flex flex-col justify-center items-center  animate-pulse">
     <Image src={Logo} alt="baro" width={280} height={280} />
