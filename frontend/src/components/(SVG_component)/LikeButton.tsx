343b7315b8651d3bbacb7f9559322a8e231e2cc7
한지웅 <wldnd2977@gmail.com>
2024-10-08 19:54:51 +0900

    feat : hear icon - wish-list api S11P21A401-597

--- frontend/src/components/(SVG_component)/LikeButton.tsx
+++ frontend/src/components/(SVG_component)/LikeButton.tsx
@@ -3,16 +3,23 @@
 import { MouseEvent, useState } from 'react';
 import colors from '@/components/colors';
 import HeartIcon from './HeartIcon';
+import {getWishDeleteApi, getWishPostApi} from "@/apis/wishListApi";
 
-export default function LikeButton({ isWished }: { isWished: boolean }) {
+export default function LikeButton({ isWished, productId }: { isWished: boolean, productId: string }) {
   // 좋아요 목록에 있는 것을 파악하고, 하트를 누른 상태로 하도록!
+  // TODO : 클릭하면 이벤트가 실행되고,  true => delete, false => post
   const isInLikeList = isWished;
   const [isClicked, setClicked] = useState(isInLikeList);
   const heartFillColor = isClicked ? colors.blue[200] : 'none';
   const handleClick = (event: MouseEvent<HTMLElement>) => {
     event.stopPropagation();
-    setClicked(!isClicked);
-    // 이곳에 API로 좋아요 리스트 반영 구현
+    if (isClicked) {
+      getWishDeleteApi(productId)
+      setClicked(false)
+    } else {
+      getWishPostApi(productId)
+      setClicked(true)
+    }
   };
 
   return (
