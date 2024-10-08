7d263c7fde2c6aa71486505061980f5599efbb74
mung96 <mung5079@gmail.com>
2024-10-08 17:36:44 +0900

    feat: 대여기간, 총대여로 로직 구현 S11P21A401-582

--- frontend/src/components/post/RentalInfoCard.tsx
+++ frontend/src/components/post/RentalInfoCard.tsx
@@ -3,44 +3,55 @@ import { HiOutlineCurrencyDollar } from 'react-icons/hi2';
 import { IoCalendarClearOutline } from 'react-icons/io5';
 import ContractWidget from '@/components/post/ContractWidget';
 import ContractCardBox from '@/components/post/ContractCardBox';
+import { ContractFormFields } from '@/hooks/post/usePostFormModel';
+import { useProfileObject } from '@/store/useMyProfile';
+import { ContractInfoStep, ContractPreviewStep, PostInfoStep, RentalInfoStep } from '@/types/domains/product';
+import { calculateDaysBetween, formatDate } from '@/utils/dayUtil';
 
-function RentalInfoCard() {
-  const [productName, setProductName] = useState('');
-  const [serialNumber, setSerialNumber] = useState('');
+type Props = {
+  fields: ContractFormFields;
+  context: PostInfoStep | RentalInfoStep | ContractInfoStep | ContractPreviewStep;
+
+}
+
+function RentalInfoCard({ fields, context }: Props) {
+  const profile = useProfileObject();
+
   return (
     <ContractCardBox title="물건 소유자 및 대여 정보" step={1}>
       <div className="flex flex-col gap-2">
         <ContractWidget
           title="물건 소유자(갑)"
           name="성명"
-          value="김소유"
+          value={profile.name}
           disabled
         />
 
         <ContractWidget
           title="대여 물품"
           name="물품 이름"
-          value={productName}
-          onChange={setProductName}
+          value={fields.productName.field.value as string}
+          onChange={fields.productName.field.onChange}
           placeholder="물건 이름"
         />
+
         <ContractWidget
           title="일련 번호"
           name="일련 번호"
-          value={serialNumber}
-          onChange={setSerialNumber}
+          value={fields.serialNumber.field.value as string}
+          onChange={fields.serialNumber.field.onChange}
           placeholder="일련 번호 입력"
         />
         <ContractWidget
           title="대여 기간"
           name={<IoCalendarClearOutline className="text-xl" />}
-          value="2024.09.24-2025.0925"
+          value={formatDate(context.rentalDuration?.from!) + ' ~ ' + formatDate(context.rentalDuration?.to!)}
           disabled
         />
         <ContractWidget
-          title="대여료"
+          title="총 대여료(원)"
           name={<HiOutlineCurrencyDollar className="text-xl" />}
-          value="60000원"
+          value={context.rentalFee! * (calculateDaysBetween(context.rentalDuration?.from!, context.rentalDuration?.to!))}
           disabled
         />
       </div>
