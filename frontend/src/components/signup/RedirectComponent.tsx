c2be54f3cd944f4f852434a47cb7e0f0c73859e0
mung96 <mung5079@gmail.com>
2024-10-08 13:29:29 +0900

    feat: 로그인시 profiel 설정 기능 추가 S11P21A401-605

--- frontend/src/components/signup/RedirectComponent.tsx
+++ frontend/src/components/signup/RedirectComponent.tsx
@@ -4,15 +4,36 @@ import Image from "next/image";
 import Logo from '@/../public/assets/png/barobaro_logo.png';
 import { useRouter, useSearchParams } from "next/navigation";
 import { jwtDecode } from "jwt-decode";
+import { getProfile } from "@/apis/profileApi";
+import { useEffect } from "react";
+import { useProfileSet } from "@/store/useMyProfile";
+import { AxiosError } from "axios";
 
 const RedirectComponent = () => {
   const searchParams = useSearchParams();
   localStorage.setItem('token',searchParams.get('token')!);
   const decoded = jwtDecode(searchParams.get('token')!);
   console.log(decoded);
+  const setProfile = useProfileSet();
+  
+  useEffect(()=>{
+    const fetchData = async () =>{
+      try{
+        const profileResponse = await getProfile();
+        setProfile({
+         ...profileResponse.data.body
+        })
+      }catch(error){
+        if(error instanceof AxiosError){
+          alert(error.response?.data.header.message)
+        }
+      }
+    }
+    fetchData();
+  })
 
-  // const router = useRouter();
-  // router.push('/home');
+  const router = useRouter();
+  router.push('/home');
 
 return <section className="w-full h-[100dvh] flex flex-col justify-center items-center  animate-pulse">
     <Image src={Logo} alt="baro" width={280} height={280} />
