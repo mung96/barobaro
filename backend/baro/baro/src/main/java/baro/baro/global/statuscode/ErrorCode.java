a04229f1c2ac197fb0efee9f877a4e9e02c739e5
박동환 <srpark13@naver.com>
2024-10-08 20:40:30 +0900

    feat : PIN 검증 관련 응답코드 추가 S11P21A401-610

--- backend/baro/baro/src/main/java/baro/baro/global/statuscode/ErrorCode.java
+++ backend/baro/baro/src/main/java/baro/baro/global/statuscode/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
     NOT_VALID_PIN_NUMBER(BAD_REQUEST.value(), "유효하지 않은 PIN입니다."),
     PRIVATE_KEY_EXCEPTION(INTERNAL_SERVER_ERROR.value(), "개인 키를 가져오는 도중 서버 에러가 발생했습니다."),
     CERTIFICATE_EXCEPTION(INTERNAL_SERVER_ERROR.value(), "개인 인증서를 가져오는 과정에서 에러가 발생했습니다."),
+    INVALID_PIN_NUMBER(BAD_REQUEST.value(), "유효하지 않은 PIN 번호입니다."),
 
     //토큰
     EXPIRED_TOKEN(BAD_REQUEST.value(), "만료된 토큰입니다."),
