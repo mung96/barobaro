f91ee324166196a624cfb0966ea9c639a764b4f8
mung96 <mung5079@gmail.com>
2024-10-08 21:59:16 +0900

    feat: 게시글 작성 api연결 S11P21A401-582

--- frontend/src/components/post/RentalInfoInput.tsx
+++ frontend/src/components/post/RentalInfoInput.tsx
@@ -15,30 +15,33 @@ type Props = {
   onPrev: () => void;
   onNext: () => void;
 
-  getValues:any;
-  handleSubmit:any;
+  getValues: any;
+  handleSubmit: any;
 
 } & StepProps<RentalFormFields>;
 
-function RentalInfoInput({ onNext, onPrev ,context,fields,errors,getValues,handleSubmit}: Props) {
-  const formatDate = (date: Date) => date.toISOString().split('T')[0];
-  const convertProductDataToRequest = ()=>{
+function RentalInfoInput({ onNext, onPrev, context, fields, errors, getValues, handleSubmit }: Props) {
+  const formatDate = (date: Date) => date?.toISOString().split('T')[0];
+  const convertProductDataToRequest = () => {
     //undefined type가드를 활용해야하는데 시간이없네
     return {
-    title: context.title!,
-    startDate: formatDate(getValues().rentalDuration?.from!),
-    endDate: formatDate(getValues().rentalDuration?.to!),
-    rentalFee: getValues().rentalFee!,
-    place: getValues().rentalAddress?.addressName!,
-    latitude: Number(getValues().rentalAddress?.latitude!),
-    longitude: Number(getValues().rentalAddress?.longitude!),
-    returnTypeList: getValues().returnTypeList!,
-    returnAddress: getValues().returnAddress?.addressName!,
-    content: context.body!,
-    category: context.category!}
+      title: context.title!,
+      startDate: formatDate(getValues().rentalDuration?.from!),
+      endDate: formatDate(getValues().rentalDuration?.to!),
+      rentalFee: getValues().rentalFee!,
+      place: getValues().rentalAddress?.addressName!,
+      latitude: Number(getValues().rentalAddress?.latitude!),
+      longitude: Number(getValues().rentalAddress?.longitude!),
+      returnTypeList: getValues().returnTypeList!,
+      returnAddress: getValues().returnAddress?.addressName!,
+      content: context.body!,
+      category: context.category!
+    }
   }
   return (
-    <form className="flex flex-col gap-4 pb-12" onSubmit={handleSubmit(()=>postProduct(convertProductDataToRequest(),context.images! as File[]))}>
+    <form className="flex flex-col gap-4 pb-12" onSubmit={() => handleSubmit(
+      postProduct(convertProductDataToRequest(), context.images! as File[])
+    )}>
       <RentalDurationInput
         value={fields.rentalDuration.field.value as DateRange}
         onSelect={fields.rentalDuration.field.onChange}
@@ -54,7 +57,7 @@ function RentalInfoInput({ onNext, onPrev ,context,fields,errors,getValues,handl
 
       <RentalAddressInput
         value={fields.rentalAddress.field.value as Location}
-        onChange={fields.rentalAddress.field.onChange}  
+        onChange={fields.rentalAddress.field.onChange}
         isInvalid={fields.rentalAddress.fieldState.invalid}
         message={errors.rentalAddress?.message!}
       />
@@ -67,7 +70,7 @@ function RentalInfoInput({ onNext, onPrev ,context,fields,errors,getValues,handl
       />
       {(fields.returnTypeList.field.value as string[])?.includes('DELIVERY') && (
         <ReturnAddressInput
-        value={fields.returnAddress.field.value as Location}
+          value={fields.returnAddress.field.value as Location}
           onChange={fields.returnAddress.field.onChange}
           isInvalid={fields.returnAddress.fieldState.invalid}
           message={errors.returnAddress?.message!}
@@ -75,12 +78,13 @@ function RentalInfoInput({ onNext, onPrev ,context,fields,errors,getValues,handl
       )}
 
       {/* <div className="flex  gap-6">*/}
-        <div className="fixed left-0 w-[100vw] bottom-0 px-4 py-3 flex gap-6 border-t-[1px] bg-white z-50">
+      <div className="fixed left-0 w-[100vw] bottom-0 px-4 py-3 flex gap-6 border-t-[1px] bg-white z-50">
         <Button onClick={onPrev} width="30%" height="48px" color="gray">
           <p className="text-base">뒤로</p>
         </Button>
 
-        <Button disabled={false} onClick={onNext} width="100%" height="48px">
+        {/* <Button disabled={false} onClick={onNext} width="100%" height="48px"> */}
+        <Button type="submit" disabled={false} width="100%" height="48px">
           <p className="text-base">다음으로 가기</p>
         </Button>
       </div>
