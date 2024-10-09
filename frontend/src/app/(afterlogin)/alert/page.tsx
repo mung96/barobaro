b6625904484c9e86e3a35f92b4baf2b720b40237
scorchedrice <woongveloper98@gmail.com>
2024-10-09 22:13:49 +0900

    fix : alert Header

--- frontend/src/app/(afterlogin)/alert/page.tsx
+++ frontend/src/app/(afterlogin)/alert/page.tsx
@@ -14,7 +14,7 @@ export default function AlertPage() {
   })
   return (
     <div>
-      <Header pageName="알람" hasPrevBtn hasSearchBtn={false} hasAlertBtn={false} chatId="temp"/>
+      <Header pageName="알람" hasPrevBtn hasSearchBtn={false} hasAlertBtn={false} />
       <section className="flex flex-col items-center w-full mb-10">
         {/*TODO : alarm data가 정상적으로 들어온다면, 해당 컴포넌트를 반복적으로 만들면 된다!*/}
         <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전' chatId="temp"/>
