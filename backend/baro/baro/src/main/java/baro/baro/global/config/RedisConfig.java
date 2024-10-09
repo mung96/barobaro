6c95359f595e74788d382a4ee0054a7832ec0a41
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-10-09 23:19:47 +0900

    feat : 메모리 누수 해결

--- backend/baro/baro/src/main/java/baro/baro/global/config/RedisConfig.java
+++ backend/baro/baro/src/main/java/baro/baro/global/config/RedisConfig.java
@@ -1,14 +1,24 @@
 package baro.baro.global.config;
 
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    private LettuceConnectionFactory lettuceConnectionFactory;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        this.lettuceConnectionFactory = new LettuceConnectionFactory();
+        return this.lettuceConnectionFactory;
+    }
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
@@ -18,6 +28,13 @@ public class RedisConfig {
         redisTemplate.setEnableTransactionSupport(true);
 
         return redisTemplate;
+    }
+
+    @PreDestroy
+    public void cleanUp() {
+        if (lettuceConnectionFactory != null) {
+            lettuceConnectionFactory.destroy();
+        }
     }
 }
 
