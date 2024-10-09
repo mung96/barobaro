134f911b0330c52b8f2a2441ee4d125b43e98afd
scorchedrice <woongveloper98@gmail.com>
2024-10-09 14:27:44 +0900

    feat : location main change (오류 확인, 추후 세부 수정)

--- frontend/src/apis/searchProductsApi.ts
+++ frontend/src/apis/searchProductsApi.ts
@@ -13,3 +13,11 @@ export const getSearchData = async (keywords: string, category: string) => {
         throw error;
     }
 }
+
+// TODO : main 동네가 어디인지 store에서 정보를 가져오고, 이를 기반으로 요청을 진행한다.
+// export const getSearchCategoryData = async (catrgory: string) => {
+//
+//     try {
+//
+//     }
+// }
--- frontend/src/store/useLocationStore.ts
+++ frontend/src/store/useLocationStore.ts
@@ -10,13 +10,19 @@ interface Location {
 
 interface LocationState {
     locations: Location[];
+    main: number;
     setLocations: (locations: Location[]) => void;
+    setMain: (main: number) => void;
 }
 
 export const useLocationStore = create<LocationState>((set) => ({
     locations: [],
+    main: 0,
     setLocations: (locations) => set({ locations }),
+    setMain: (main) => set({main})
 }));
 
 export const useLocations = () => useLocationStore((state) => state.locations);
+export const useMain = () => useLocationStore((state) => state.main);
 export const useSetLocations = () => useLocationStore((state) => state.setLocations);
+export const useSetMain = () => useLocationStore((state) => state.setMain);
