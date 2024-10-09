771540e59d38fdc0513d363962aa9f61a20c1dd1
scorchedrice <woongveloper98@gmail.com>
2024-10-09 21:49:57 +0900

    feat : alert 구현 기능 (api 연동후 세부 조정 필요)

--- frontend/src/app/(afterlogin)/alert/page.tsx
+++ frontend/src/app/(afterlogin)/alert/page.tsx
@@ -14,8 +14,15 @@ export default function AlertPage() {
   return (
     <div>
       <Header pageName="알람" hasPrevBtn hasSearchBtn={false} hasAlertBtn={false}/>
-      <section className="flex flex-col items-center w-full mb-20">
-        <AlertComponent/>
+      <section className="flex flex-col items-center w-full mb-10">
+        {/*TODO : alarm data가 정상적으로 들어온다면, 해당 컴포넌트를 반복적으로 만들면 된다!*/}
+        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전'/>
+        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전'/>
+        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전'/>
+        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전'/>
+        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전'/>
+        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전'/>
+        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전'/>
       </section>
     </div>
   )
