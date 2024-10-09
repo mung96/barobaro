9a7b33462872a953dfe928038551b9a5cd886075
scorchedrice <woongveloper98@gmail.com>
2024-10-09 21:49:57 +0900

    feat : alert 구현 기능 (api 연동후 세부 조정 필요)

--- frontend/src/components/alert/AlertComponent.tsx
+++ frontend/src/components/alert/AlertComponent.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import Baro from '@/../public/assets/png/barobaro_logo.png'
 
-export default function AlertComponent() {
-  const nicknameTemp = 'BiBimKing';
+// TODO : TimeData 무엇 올지 모름.
+type Props = {
+  profileUrl: string;
+  nickname: string;
+  orderType: string;
+  message: string;
+  timeData: any;
+}
+
+export default function AlertComponent( {nickname, profileUrl, orderType, message, timeData} : Props) {
+
   return (
-    <div className="flex bg-gray-500 w-[90%] h-[100px] rounded-[10px]">
+    <div className="flex bg-gray-500 w-[90%] h-[100px] rounded-[10px] my-2">
       <div className="flex justify-center items-center w-[70px]">
         <div className="rounded-full w-[50px] h-[50px] overflow-hidden">
           <Image src={Baro} alt='bar' width={50} height={50} />
@@ -13,19 +22,19 @@ export default function AlertComponent() {
       <div className="flex flex-col flex-1 mt-3">
         <div className="flex w-full">
           <div className="text-blue-300 font-bold ml-3">
-            계약 요청
+            {orderType}
           {/* TODO : 오는 값에 따라 다른 값 */}
           </div>
           <div className="flex-1"/>
           <div className="text-gray-300 mr-3">
-            1분전
+            {timeData}
           {/*  TODO : 오는 시간 값 활용 */}
           </div>
         </div>
         <div className="flex mx-3 items-start">
           <div className="text-black-100 text-[14px]">
-            <span>{nicknameTemp}</span>
-            님이 서명을 요청하셨습니다.
+            <span>{nickname}</span>
+            {message}
           </div>
         </div>
         <button type="button" className="flex justify-center items-center rounded-[7px] bg-gray-600 text-gray-100 w-[60px] h-[20px] text-[10px] ml-3 mt-1">
