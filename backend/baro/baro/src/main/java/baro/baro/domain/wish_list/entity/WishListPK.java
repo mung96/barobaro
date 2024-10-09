d86f5dafa148d6232403a7d3731d0d9194adf81a
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-10-09 23:20:29 +0900

    feat : 관심내역 추가 삭제 조회 API 구현

--- backend/baro/baro/src/main/java/baro/baro/domain/wish_list/entity/WishListPK.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/wish_list/entity/WishListPK.java
@@ -1,5 +1,10 @@
 package baro.baro.domain.wish_list.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class WishListPK {
     private Long memberId;
 
