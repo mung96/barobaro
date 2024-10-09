ab5c1c5727481a13efbc7d649174d5178b0b39ef
scorchedrice <woongveloper98@gmail.com>
2024-10-09 14:27:44 +0900

    feat : location main change (오류 확인, 추후 세부 수정)

--- frontend/src/components/user/UserTown.tsx
+++ frontend/src/components/user/UserTown.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { getLocation, changeMainLocation } from "@/apis/profileApi";
-import { useSetLocations, useLocations } from '@/store/useLocationStore';
+import { getLocation } from "@/apis/profileApi";
+import { postDefaultLocation } from "@/apis/locationApi"
+import {useSetLocations, useLocations, useSetMain} from '@/store/useLocationStore';
 import Location from "@/components/(SVG_component)/Location";
 
 export default function UserTown() {
   const locations = useLocations();
   const setLocations = useSetLocations();
+  const setMainLocation = useSetMain();
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
@@ -15,6 +17,7 @@ export default function UserTown() {
       try {
         const response = await getLocation();
         setLocations(response);
+
       } catch (error) {
         console.error("위치 정보를 가져오는데 실패했습니다:", error);
       }
@@ -22,16 +25,17 @@ export default function UserTown() {
     fetchLocations();
   }, [setLocations]);
 
-  // const handleChangeMainLocation = async (locationId: number) => {
-  //   try {
-  //     await changeMainLocation();
-  //     const updatedLocations = await getLocation();
-  //     setLocations(updatedLocations);
-  //     setIsOpen(false);
-  //   } catch (error) {
-  //     console.error("메인 위치 변경에 실패했습니다:", error);
-  //   }
-  // };
+  const handleChangeMainLocation = async (locationId: number) => {
+    try {
+      await postDefaultLocation(locationId);
+      const updatedLocations = await getLocation();
+      setMainLocation(locationId)
+      setLocations(updatedLocations);
+      setIsOpen(false);
+    } catch (error) {
+      console.error("메인 위치 변경에 실패했습니다:", error);
+    }
+  };
 
   const mainLocation = locations.find(location => location.isMain) || locations[0];
 
@@ -44,18 +48,18 @@ export default function UserTown() {
             className={`flex justify-center items-center px-4 py-2 text-sm font-medium text-gray-700 rounded-[10px] ${isOpen ? 'bg-gray-400' : null}`}
           >
             <Location where={mainLocation.dong} />
-            {/*{mainLocation ? mainLocation.dong : '위치를 선택하세요'}*/}
-            {/*<span className="ml-2">▼</span>*/}
           </button>
 
           {isOpen && (
-            <div className="absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-              <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                {locations.map((location) => (
+            <div className="absolute left-0 mt-2 w-[150px] rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+              <div className="py-1" role="menu">
+                {locations
+                .filter(location => !location.isMain)
+                .map((location) => (
                   <button
                     key={location.locationId}
-                    // onClick={() => handleChangeMainLocation(location.locationId)}
-                    className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                    onClick={() => handleChangeMainLocation(location.locationId)}
+                    className="block text-left px-4 py-2 text-sm text-gray-700"
                     role="menuitem"
                   >
                     {location.dong}
