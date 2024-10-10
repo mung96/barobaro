501a36917e2d927c61238d6df173ca0f4e0e49b1
이주영 <git.leej.young@gmail.com>
2024-10-11 02:54:17 +0900

    chore: Image 태그 -> img 태그로 변경 S11P21A401-672

--- frontend/src/components/message/ChatRoom.tsx
+++ frontend/src/components/message/ChatRoom.tsx
@@ -4,31 +4,19 @@ import Image from 'next/image';
 import { MouseEvent } from 'react';
 import ChatRoomType from './ChatRoomType';
 
-export default function ChatRoomEach({
-  chatRoomId,
-  profileImage,
-  nickname,
-  lastChat,
-  productMainImage,
-}: ChatRoomType) {
+export default function ChatRoomEach({ chatRoomId, profileImage, nickname, lastChat, productMainImage }: ChatRoomType) {
   const isClicked = (e: MouseEvent<HTMLElement>) => {
     e.currentTarget.classList.add('bg-gray-200');
   };
 
   return (
-    <Link
-      className="flex gap-0 p-4"
-      href={encodeURI(`message/chat/${chatRoomId}`)}
-      onMouseDown={isClicked}
-    >
+    <Link className="flex gap-0 p-4" href={encodeURI(`message/chat/${chatRoomId}`)} onMouseDown={isClicked}>
       <div className="w-2/12 flex items-center justify-center">
         <div className="relative w-full h-full aspect-square">
-          <Image
-            src={profileImage} // public 폴더 내의 이미지 경로
-            alt="profImg" // 이미지 설명 추가
-            layout="fill" // 부모 div의 크기에 맞게 이미지가 채워지도록 설정
-            objectFit="cover" // 이미지 비율 유지하면서 부모 div에 맞게 채움
-            className="rounded-full" // 원형으로 만들기
+          <img
+            src={profileImage}
+            alt="profImg"
+            className="rounded-full w-full h-full object-cover" // 원형으로 만들기 및 부모 div에 맞게 채우기
           />
         </div>
       </div>
@@ -38,20 +26,16 @@ export default function ChatRoomEach({
         <div className="flex items-center">
           <span className="font-bold inline-block">{nickname}</span>
         </div>
-        <p className="text-xs overflow-hidden text-ellipsis whitespace-nowrap">
-          {lastChat}
-        </p>
+        <p className="text-xs overflow-hidden text-ellipsis whitespace-nowrap">{lastChat}</p>
       </div>
 
       {/* 오른쪽 이미지 */}
       <div className="w-2/12 flex items-center justify-center">
         <div className="relative w-full h-full">
-          <Image
+          <img
             src={productMainImage} // public 폴더 내의 이미지 경로
             alt="thumb" // 이미지 설명 추가
-            layout="fill" // 부모 div의 크기에 맞게 이미지가 채워지도록 설정
-            objectFit="cover" // 이미지 비율 유지하면서 부모 div에 맞게 채움
-            className="rounded-lg" // 원형으로 만들기
+            className="rounded-lg w-full h-full object-cover" // 모서리 둥글게 및 부모 div에 맞게 채우기
           />
         </div>
       </div>
