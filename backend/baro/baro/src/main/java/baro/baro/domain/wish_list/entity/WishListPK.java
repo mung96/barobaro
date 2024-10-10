25e439695a5e28163627873ebe7b083762d89441
정지연 <jung_j_yeon@naver.com>
2024-10-10 11:56:32 +0900

    feat : WishList 생성자 추가 S11P21A401-648

--- backend/baro/baro/src/main/java/baro/baro/domain/wish_list/entity/WishListPK.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/wish_list/entity/WishListPK.java
@@ -1,9 +1,15 @@
 package baro.baro.domain.wish_list.entity;
 
+import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Data
+@Embeddable
+@NoArgsConstructor
 @AllArgsConstructor
 public class WishListPK {
     private Long memberId;
