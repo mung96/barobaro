e1e4fdc8d0eb2e7894d23b110aba00d56e292f73
이윤지 <yunji383614@gmail.com>
2024-10-10 14:26:58 +0900

    feat : 계좌 조회 API 설계 S11P21A401-633

--- backend/baro/baro/src/main/java/baro/baro/domain/account/dto/AccountDto.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/account/dto/AccountDto.java
@@ -1,5 +1,6 @@
 package baro.baro.domain.account.dto;
 
+import baro.baro.domain.account.entity.Account;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,4 +14,13 @@ public class AccountDto {
     private Long accountId;
 
     private Boolean main;
+
+    public static AccountDto toDto(Account account) {
+        return AccountDto.builder()
+                .bank(account.getBank())
+                .accountNumber(account.getAccountNumber())
+                .accountId(account.getId())
+                .main(account.getMain())
+                .build();
+    }
 }
