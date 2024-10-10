005776979ac58cf2baafb429dcbc4d033d01765c
정지연 <jung_j_yeon@naver.com>
2024-10-11 01:26:45 +0900

    fix : 기본 생성자 추가 S11P21A401-667

--- backend/baro/baro/src/main/java/baro/baro/domain/contract/dto/ContractApplicationDto.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/contract/dto/ContractApplicationDto.java
@@ -9,12 +9,14 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ContractApplicationDto {
 
     private Long chatRoomId;
