61e639c7949ccdc9e4847098ed6f13cb1dec45e9
정지연 <jung_j_yeon@naver.com>
2024-10-10 17:17:44 +0900

    fix : import문 에러해결 S11P21A401-660

--- backend/baro/baro/src/main/java/baro/baro/domain/contract/repository/SignatureInformationRepository.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/contract/repository/SignatureInformationRepository.java
@@ -2,8 +2,10 @@ package baro.baro.domain.contract.repository;
 
 import baro.baro.domain.contract.entity.SignatureInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
