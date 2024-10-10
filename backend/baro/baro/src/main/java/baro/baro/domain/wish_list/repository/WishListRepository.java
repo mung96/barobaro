c125bbf3a04a142bed78ea8d5de98a16273d1a84
정지연 <jung_j_yeon@naver.com>
2024-10-10 15:13:28 +0900

    feat : 물품 삭제 시 연관 entity 전부 삭제 하는 함수 추가 S11P21A401-547

--- backend/baro/baro/src/main/java/baro/baro/domain/wish_list/repository/WishListRepository.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/wish_list/repository/WishListRepository.java
@@ -1,17 +1,12 @@
 package baro.baro.domain.wish_list.repository;
 
-import baro.baro.domain.product.dto.SearchProductTmpDto;
-import baro.baro.domain.product.entity.Category;
 import baro.baro.domain.wish_list.entity.WishList;
 import baro.baro.domain.wish_list.entity.WishListPK;
 import io.lettuce.core.dynamic.annotation.Param;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 public interface WishListRepository extends JpaRepository<WishList, WishListPK> {
     @Query("SELECT COUNT(w) > 0 " +
@@ -25,9 +20,15 @@ public interface WishListRepository extends JpaRepository<WishList, WishListPK> 
             "WHERE w.product.id = :productId ")
     Integer countWishList(@Param("productId") Long productId);
 
-    @Transactional
     @Modifying
+    @Transactional
     @Query("DELETE FROM WishList w " +
             "WHERE w.member.id = :memberId AND w.product.id = :productId")
     void deleteByMemberIdAndProductId(@Param("memberId") Long memberId, @Param("productId") Long productId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM WishList w " +
+            "WHERE w.product.id = :productId")
+    void deleteWishListByProductId(@Param("productId") Long productId);
 }
