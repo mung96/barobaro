07e412c822e6eebcbd34e44e32e15c910ffbae68
한지웅 <wldnd2977@gmail.com>
2024-10-10 15:36:20 +0900

    feat : alert component S11P21A401-636

--- frontend/src/app/(afterlogin)/alert/page.tsx
+++ frontend/src/app/(afterlogin)/alert/page.tsx
@@ -1,30 +1,38 @@
 'use client';
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "@/components/Header";
 import {getAlerts} from "@/apis/alertApi";
 import AlertComponent from "@/components/alert/AlertComponent";
 
 export default function AlertPage() {
+  const [alertData, setAlertData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    // TODO : 해당 페이지가 랜더링되면, 알람이 무엇 있는지 확인해야합니다.
-    // TODO : 혹은, home으로 갈 때, store에 저장하고 있다가, 이를 빼서 써야합니다.
-    console.log('ALERT PAGE')
-    const res = getAlerts()
-  })
+    const getAlertFunction = async () => {
+      try {
+        setIsLoading(true);
+        const res = await getAlerts();
+        console.log('@@',res);
+        setAlertData(res.data.body.notifications || []);
+      } catch (error) {
+        console.error("Error fetching alerts:", error);
+        setAlertData([]);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getAlertFunction();
+  }, []); // 빈 의존성 배열을 사용하여 컴포넌트 마운트 시 한 번만 실행
   return (
     <div>
       <Header pageName="알람" hasPrevBtn hasSearchBtn={false} hasAlertBtn={false} />
       <section className="flex flex-col items-center w-full mb-10">
         {/*TODO : alarm data가 정상적으로 들어온다면, 해당 컴포넌트를 반복적으로 만들면 된다!*/}
-        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전' chatId="temp"/>
-        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전' chatId="temp"/>
-        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전' chatId="temp"/>
-        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전' chatId="temp"/>
-        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전' chatId="temp"/>
-        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전' chatId="temp"/>
-        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전' chatId="temp"/>
-        <AlertComponent nickname="BiBimking" message='님이 비빔을 신청하셨습니다' orderType="비빔 신청" profileUrl='tempUrl' timeData='1분전' chatId="temp"/>
+        {alertData.map((data, index) => (
+            <AlertComponent key={index} nickname={data.fromMemberNickName} message={data.message} orderType={data.notiType} profileUrl={data.fromMemberImage} timeData='1' chatId='2'/>
+        ))}
       </section>
     </div>
   )
