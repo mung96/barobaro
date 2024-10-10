030c5fb54d272618f11132d36f64dd0122e8e891
정지연 <jung_j_yeon@naver.com>
2024-10-10 15:13:28 +0900

    feat : 물품 삭제 시 연관 entity 전부 삭제 하는 함수 추가 S11P21A401-547

--- backend/baro/baro/src/main/java/baro/baro/domain/contract/repository/ContractRepository.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/contract/repository/ContractRepository.java
@@ -2,6 +2,11 @@ package baro.baro.domain.contract.repository;
 
 import baro.baro.domain.contract.entity.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
+    @Query("SELECT c FROM Contract c " +
+            "WHERE c.product.id = :productId")
+    Contract findContractByProductId(@Param("productId") Long productId);
 }
--- backend/baro/baro/src/main/java/baro/baro/domain/product_image/repository/ProductImageRepository.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/product_image/repository/ProductImageRepository.java
@@ -3,7 +3,9 @@ package baro.baro.domain.product_image.repository;
 import baro.baro.domain.product_image.entity.ProductImage;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,4 +21,10 @@ public interface ProductImageRepository extends JpaRepository<ProductImage, Long
             "WHERE pi.product.id = :productId " +
             "AND pi.isMain = true")
     String findMainImageUrl(@Param("productId") Long productId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM ProductImage pi " +
+            "WHERE pi.product.id = :productId")
+    void deleteByProductId(@Param("productId") Long productId);
 }
