931edaf68c8384e12ca8b7483cbc905586862994
박동환 <srpark13@naver.com>
2024-10-10 16:51:54 +0900

    feat : 빌려준,빌린 물품 리스트 API 구현 S11P21A401-655

--- backend/baro/baro/src/main/java/baro/baro/domain/contract/repository/SignatureInformationRepository.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/contract/repository/SignatureInformationRepository.java
@@ -2,6 +2,12 @@ package baro.baro.domain.contract.repository;
 
 import baro.baro.domain.contract.entity.SignatureInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SignatureInformationRepository extends JpaRepository<SignatureInformation, Long> {
+    @Query("SELECT s FROM SignatureInformation s WHERE s.contract.id = :contractId")
+    List<SignatureInformation> findByContractId(@Param("contractId") Long contractId);
 }
