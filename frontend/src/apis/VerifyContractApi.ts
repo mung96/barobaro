7b7b76adf543813c6151edbc92dbefcca4b86bb7
scorchedrice <woongveloper98@gmail.com>
2024-10-11 01:51:14 +0900

    feat : get Contract Pdf File Api

---
+++ frontend/src/apis/VerifyContractApi.ts
@@ -0,0 +1,7 @@
+// TODO : 멀티파트, file로 pdf 첨부, get
+
+// export const getVerifyContract = async (PdfFile, paperId) => {
+//   const formData = new FormData();
+//   formData.append('', paperId);
+//   formData.append('paperId', paperId);
+// }
