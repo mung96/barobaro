7065ad06b3c2ca2881a506a44c854388a5e72592
정지연 <jung_j_yeon@naver.com>
2024-10-11 01:13:28 +0900

    feat : 알림 리스트 조회 API 구현 S11P21A401-667

--- backend/baro/baro/src/main/java/baro/baro/domain/noti/repository/NotiRepository.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/noti/repository/NotiRepository.java
@@ -1,7 +1,20 @@
 package baro.baro.domain.noti.repository;
 
+import baro.baro.domain.noti.dto.response.NotiDto;
 import baro.baro.domain.noti.entity.Noti;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface NotiRepository extends JpaRepository<Noti, Long> {
+    @Query("SELECT new baro.baro.domain.noti.dto.response.NotiDto(n.message, n.fromMember.uuid," +
+            "n.fromMember.profileImage, n.fromMember.nickname, n.notiType) " +
+            "FROM Noti n " +
+            "WHERE n.toMember.id = :memberId " +
+            "ORDER BY n.id DESC")
+    List<NotiDto> findNotisByMemberId(@Param("memberId") Long memberId, Pageable pageable);
+
 }
