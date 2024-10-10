fdc5a96528f1a3607b903ace63c2802e4da9ca42
한지웅 <wldnd2977@gmail.com>
2024-10-10 18:47:24 +0900

    design : verify S11P21A401-664

--- frontend/src/app/(afterlogin)/mypage/verify/page.tsx
+++ frontend/src/app/(afterlogin)/mypage/verify/page.tsx
@@ -4,28 +4,38 @@ export default function VerifyPage() {
     return (
         <div>
             <Header pageName="마이페이지" hasPrevBtn hasSearchBtn={false} hasAlertBtn />
-            <h1 className="font-bold text-[15px]">문서 검증 센터</h1>
-            <div className="text-[10px]">
-                <p>문서가 원본인지 걱정하셨나요?</p>
-                <p>원본 검증키 대조로 문서의 위·변조 여부를 누구나 쉽게 확인할 수 있습니다.</p>
-            </div>
-            <div>
-                <div>
-                <div className="text-[12px] font-bold">문서 ID</div>
-                    <div>문서 ID 확인 방법</div>
+            <section className="mt-[80px] flex flex-col items-center">
+                <div className="flex flex-col items-center">
+                    <h1 className="font-bold text-[15px]">문서 검증 센터</h1>
+                    <div className="text-[10px] m-5 items-center flex-col flex">
+                        <p>문서가 원본인지 걱정하셨나요?</p>
+                        <p>원본 검증키 대조로 문서의 위·변조 여부를 누구나 쉽게 확인할 수 있습니다.</p>
+                    </div>
                 </div>
-                {/*TODO : 색상 칠하고, REGexp*/}
-                <div>영문과 숫자, -만 입력</div>
-            </div>
-            <div>
-                <h2 className="text-[12px] font-bold">검증 대상 문서</h2>
-                <div>
-                    문서 검색 및 찾기
-                </div>
-            </div>
-            <div>
-                검증하기
-            </div>
+                <section className="w-[90%] flex flex-col items-center">
+                    <div className="w-[85%]">
+                        <div className="flex justify-between">
+                            <div className="text-[12px] font-bold">문서 ID</div>
+                            <div className="font-bold text-[8px] text-gray-600 underline">문서 ID 확인 방법</div>
+                        </div>
+                        {/*TODO: input으로 변경하여 값 삽입*/}
+                    </div>
+                    <div
+                        className="bg-gray-400 rounded-[5px] text-gray-600 text-[10px] w-[300px] h-[35px] flex flex-col justify-center my-3">영문과
+                        숫자,
+                        -만 입력
+                    </div>
+                    <div className="mt-3 w-[85%]">
+                        <h2 className="text-[12px] font-bold">검증 대상 문서</h2>
+                    </div>
+                    <div className="bg-gray-400 rounded-[5px] text-gray-600 text-[10px] w-[300px] h-[170px] flex flex-col justify-center items-center my-3">
+                        파일 찾기?
+                    </div>
+                    <button type="button" className="font-bold bg-blue-100 w-[130px] h-[30px] text-white text-xs rounded-[8px]">
+                        검증하기
+                    </button>
+                </section>
+            </section>
         </div>
     )
 }
