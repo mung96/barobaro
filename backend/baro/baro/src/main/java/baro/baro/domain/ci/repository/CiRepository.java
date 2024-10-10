3f7fa6f0b93281298fb32f026db21bb7f8fb8518
정지연 <jung_j_yeon@naver.com>
2024-10-10 19:37:44 +0900

    feat : ci 있는지 조회하는 쿼리 수정 S11P21A401-660

--- backend/baro/baro/src/main/java/baro/baro/domain/ci/repository/CiRepository.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/ci/repository/CiRepository.java
@@ -1,9 +1,13 @@
 package baro.baro.domain.ci.repository;
 
 import baro.baro.domain.ci.entity.Ci;
-import baro.baro.domain.member.entity.Member;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CiRepository extends JpaRepository<Ci, Long> {
-    boolean findCiByMember(Member member);
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END " +
+            "FROM Ci c " +
+            "WHERE c.member.id = :memberId")
+    boolean existsCiByMember(@Param("memberId") Long memberId);
 }
--- backend/baro/baro/src/main/java/baro/baro/global/feigin_client/service/PortOneFeignClientCustom.java
+++ backend/baro/baro/src/main/java/baro/baro/global/feigin_client/service/PortOneFeignClientCustom.java
@@ -1,20 +1,18 @@
 package baro.baro.global.feigin_client.service;
 
 import baro.baro.global.feigin_client.dto.request.PortOneTokenReq;
-import baro.baro.global.feigin_client.dto.response.NaverSearchRes;
 import baro.baro.global.feigin_client.dto.response.PortOneCiRes;
 import baro.baro.global.feigin_client.dto.response.PortOneTokenRes;
-import lombok.Getter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "portOneFeignClientCustom", url = "https://api.iamport.kr/")
+@FeignClient(name = "portOneFeignClientCustom", url = "https://api.iamport.kr")
 public interface PortOneFeignClientCustom {
-    @PostMapping(value = "users/getToken", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/users/getToken", consumes = MediaType.APPLICATION_JSON_VALUE)
     PortOneTokenRes getToken(@RequestBody PortOneTokenReq request);
 
-    @GetMapping(value = "certifications/{imp_uid}" , produces = MediaType.APPLICATION_JSON_VALUE)
-    PortOneCiRes getCi(@PathVariable String imp_uid,
+    @GetMapping(value = "/certifications/{imp_uid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    PortOneCiRes getCi(@PathVariable("imp_uid") String imp_uid,
                        @RequestHeader("Authorization") String authorization);
 }
