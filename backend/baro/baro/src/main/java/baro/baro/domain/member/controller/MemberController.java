89eef772947ea7b1bc30f9a03bdab5b0c71a4ca8
이윤지 <yunji383614@gmail.com>
2024-10-10 15:53:49 +0900

    feat : 프로필 수정 API 설계 S11P21A401-652

--- backend/baro/baro/src/main/java/baro/baro/domain/member/controller/MemberController.java
+++ backend/baro/baro/src/main/java/baro/baro/domain/member/controller/MemberController.java
@@ -84,8 +84,10 @@ public class MemberController {
 
     @PostMapping("/members/me/profile")
     public ResponseEntity<?> profileModify(@RequestPart(value = "dto") ProfileModifyReq profileModifyReq,
-                                           @RequestPart(value = "file") MultipartFile file) {
-        ProfileDetailsRes result = new ProfileDetailsRes("프로필 이미지", "uuid", "닉네임", "010-1111-1111", "ssafy@ssafy.com", "아무개",Boolean.TRUE);
+                                           @RequestPart(value = "file") MultipartFile file) throws IOException {
+        Long memberId = jwtService.getUserId(SecurityContextHolder.getContext());
+
+        ProfileDetailsRes result = memberService.modifyProfie(memberId, profileModifyReq, file);
 
         return new ResponseEntity<>(ResponseDto.success(PROFILE_MODIFIED, result), OK);
     }
