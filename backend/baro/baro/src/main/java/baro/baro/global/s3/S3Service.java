31f3af8bcd4ac3004c63e5519a42c0955c290c51
정지연 <jung_j_yeon@naver.com>
2024-10-11 05:40:50 +0900

    fix : 디버깅을 위한 로그 추가

--- backend/baro/baro/src/main/java/baro/baro/global/s3/S3Service.java
+++ backend/baro/baro/src/main/java/baro/baro/global/s3/S3Service.java
@@ -57,7 +57,7 @@ public abstract class S3Service {
     }
 
     protected Date getlastModified(String fileName){
-        log.info("getlastModified 시작");
+        log.info("getlastModified 시작" + fileName);
         ObjectMetadata objectMetadata = amazonS3Client.getObjectMetadata(bucket, fileName);
         log.info("getlastModified 들옴");
         log.info(objectMetadata.getLastModified().toString());
