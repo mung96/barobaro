e9d79b483f6cdb70263c2c5a78b0ff9ce0cd0593
한지웅 <wldnd2977@gmail.com>
2024-10-10 10:39:02 +0900

    account select 로직 수정 S11P21A401-640

--- frontend/src/apis/wishListApi.ts
+++ frontend/src/apis/wishListApi.ts
@@ -1,9 +1,15 @@
 import {axiosInstance} from "@/apis/axiosInstance";
 import {END_POINT} from "@/constants/api";
 
-export const getWishPostApi = async (productId: string) => {
+// TODO : APi 문서 확인 후 , 값 및 params 수정
+export const getWishPostApi = async (productId: string, addOrDelete: boolean) => {
     try {
-        const response = await axiosInstance.post(END_POINT.WISH_LIST + '/' + productId);
+        const response = await axiosInstance.post(END_POINT.WISH_LIST, {
+            params: {
+                productId,
+                addOrDelete,
+            }
+        });
         console.log('POST 성공', response);
         return response.headers; // 응답 데이터 반환
     } catch (error) {
@@ -12,13 +18,13 @@ export const getWishPostApi = async (productId: string) => {
     }
 }
 
-export const getWishDeleteApi = async (productId: string) => {
-    try {
-        const response = await axiosInstance.delete(END_POINT.WISH_LIST + '/' + productId);
-        console.log('DELETE 성공', response);
-        return response; // 응답 데이터 반환
-    } catch (error) {
-        console.error('POST 요청 중 오류 발생:', error);
-        throw error; // 오류를 호출자에게 전달
-    }
-}
+// export const getWishDeleteApi = async (productId: string) => {
+//     try {
+//         const response = await axiosInstance.delete(END_POINT.WISH_LIST + '/' + productId);
+//         console.log('DELETE 성공', response);
+//         return response; // 응답 데이터 반환
+//     } catch (error) {
+//         console.error('POST 요청 중 오류 발생:', error);
+//         throw error; // 오류를 호출자에게 전달
+//     }
+// }
